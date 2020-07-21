70a40528e8f6921f7f83fdd1fece6660f847788f
alessia <alessia.cerami@studio.unibo.it>
2020-07-21 18:19:24 +0200

    Added load game page

--- src/vuejs/src/router/index.js
+++ src/vuejs/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home/Homepage.vue'
+import Join from '../views/Game/loadGame/loadGame.vue'
 import newGame from '../views/Game/newGame/newGame.vue'
 
 Vue.use(VueRouter)
@@ -16,6 +17,11 @@ Vue.use(VueRouter)
     name: 'new_game',
     component: newGame
   },
+  {
+    path: '/load_game',
+    name: 'load_game',
+    component: Join
+  }
 ]
 
 const router = new VueRouter({
