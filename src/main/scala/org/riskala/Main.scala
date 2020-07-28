28329fde63c7e96a67dc7dfc6b84c7297bf0793c
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-07-29 00:58:42 +0200

    Basic akka-http webserver serving vue app

--- src/main/scala/org/riskala/Main.scala
+++ src/main/scala/org/riskala/Main.scala
@@ -1,5 +1,35 @@
 package org.riskala
 
+import scala.io.StdIn
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.ContentTypeResolver.Default
+import akka.http.scaladsl.model.StatusCodes
+import akka.stream.ActorMaterializer
+
 object Main extends App {
-  print("Hello World!")
+
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.dispatcher
+
+  val staticResources =
+    (get & pathPrefix("")){
+      (pathEndOrSingleSlash & redirectToTrailingSlashIfMissing(StatusCodes.TemporaryRedirect)) {
+        getFromResource("static/index.html")
+      } ~ {
+        getFromResourceDirectory("static")
+      }
+    }
+
+  val bindingFuture = Http().bindAndHandle(staticResources, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
 }
+
