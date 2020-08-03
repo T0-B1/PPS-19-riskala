3a7123db2b2722b364bf71d44f121b9e999ffd85
alessia <alessia.cerami@studio.unibo.it>
2020-08-03 11:11:06 +0200

    added region javadoc

--- src/main/scala/org/riskala/Model/Region.scala
+++ src/main/scala/org/riskala/Model/Region.scala
@@ -2,8 +2,18 @@ package org.riskala.Model
 
 import org.riskala.Model.State.State
 
-case class Region(states: List[State],
-                  bonus: Int) {
 
+/** Region structure
+ *
+ * @param states       list of states
+ * @param bonus        bonus
+ */
+case class Region(states: List[State], bonus: Int) {
+
+  /**
+   * check if the region contains the given state
+   *
+   * @param state     the state to check
+   * @return true if it contains state*/
   def hasState(state: State): Boolean = states.contains(state)
 }
