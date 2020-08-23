c7a1b234e04a01409711ddd791587fc723cee832
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-08-23 04:05:17 +0200

    Replaced deprecated method in server binding

--- src/main/scala/org/riskala/controller/Main.scala
+++ src/main/scala/org/riskala/controller/Main.scala
@@ -23,11 +23,12 @@ object Main extends App {
         getFromResourceDirectory("static")
       }
     }
-
-  val bindingFuture = Http().bindAndHandle(staticResources, "localhost", 8080)
+  
+  val bindingFuture = Http().newServerAt("localhost", 8080).bindFlow(staticResources)
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
+
   bindingFuture
     .flatMap(_.unbind()) // trigger unbinding from the port
     .onComplete(_ => system.terminate()) // and shutdown when done
