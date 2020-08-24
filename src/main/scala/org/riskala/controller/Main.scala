4cd0a53b7457bb0bd744b0a36234a77a9cf69ba2
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-08-24 03:25:51 +0200

    Managing websocket with source+sink instead of flow

--- src/main/scala/org/riskala/controller/Main.scala
+++ src/main/scala/org/riskala/controller/Main.scala
@@ -1,13 +1,19 @@
 package org.riskala.controller
 
+import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
+import akka.actor.typed.ActorRef
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.AttributeKeys.webSocketUpgrade
 import akka.http.scaladsl.model.HttpMethods.GET
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes, Uri}
 import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
+import akka.stream.{CompletionStrategy, OverflowStrategy}
 import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.typed.scaladsl.ActorSource
+
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.io.StdIn
 
@@ -33,7 +39,9 @@ object Main extends App {
       req.attribute(webSocketUpgrade) match {
         case Some(upgrade) => req.uri.query().get("token") match {
           case Some(token) => {
-            upgrade.handleMessages(webSocketHandler(token))
+            //upgrade.handleMessages(webSocketHandler(token))
+            val (sourceActor, newSource) = source.preMaterialize()
+            upgrade.handleMessagesWithSinkSource(sink(token), newSource)
           }
           case None => HttpResponse(400, entity = "Missing token!")
         }
@@ -58,6 +66,7 @@ object Main extends App {
         // this means we might start sending the response even before the
         // end of the incoming message has been received
         case tm: TextMessage => {
+
           tm.textStream.runWith(Sink.fold[String, String]("")(_ + _))
             .onComplete(s => println(s"Received: $s from $token"))
           TextMessage(Source.single("Hello ") ++ tm.textStream) :: Nil
@@ -74,8 +83,6 @@ object Main extends App {
         _.withPeriodicKeepAliveMode("pong")
          .withPeriodicKeepAliveMaxIdle(1.second)))
     .bindSync(webSocketRequestHandler)
-
-  //val bindingFuture = Http().newServerAt("localhost", 8080).bindFlow(staticResourcesHandler)
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
