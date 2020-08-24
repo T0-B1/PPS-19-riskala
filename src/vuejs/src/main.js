17003254bef4bede54d1ddcff4189433f9133f7d
alessia <alessia.cerami@studio.unibo.it>
2020-08-24 17:45:45 +0200

    Added store

--- src/vuejs/src/main.js
+++ src/vuejs/src/main.js
@@ -1,14 +1,43 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import Vuex from 'vuex';
 import './custom.sass'
 import BootstrapVue from 'bootstrap-vue'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
+Vue.use(Vuex);
 
 Vue.config.productionTip = false
+
+
+const store = new Vuex.Store({
+  state: {
+    isLogged: false,
+  },
+  mutations: {
+    login(state) {
+      state.isLogged = true;
+    },
+    logout(state) {
+      state.isLogged = false;
+    }
+  }
+});
+
+router.beforeEach((to, from, next) => {
+  const realRoute = ['/', '/create_room', '/room', '/game', '/login', '/registration'];
+  const loggedRoute = ['/create_room', '/room', '/game', '/'];
+  const p = to.path;
+  if (!realRoute.includes(p)) {
+    next('/');
+  } else if (loggedRoute.includes(p) && store.state.isLogged === 'false') {
+    next('/login');
+  } else {
+    next();
+  }
+});
 
 new Vue({
   router,
