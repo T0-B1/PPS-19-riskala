49f92f5d34fe2c52e576e51aa86f74b5874e1a26
Andrea <andrea.giordano5@studio.unibo.it>
2020-08-25 18:18:39 +0200

    Added token generation and registration

--- src/main/scala/org/riskala/controller/AuthManager.scala
+++ src/main/scala/org/riskala/controller/AuthManager.scala
@@ -4,19 +4,39 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.DefaultJsonProtocol
 
 import scala.collection.immutable.HashMap
-
-object AuthManager {
-  private var credential: HashMap[String,String] = HashMap("Test"->"1234")
-  def login(l: Login): Option[String] = {
-    credential get l.username flatMap(psw => if(psw == l.password) Some("token123ABC") else None)
-  }
-
-  def register(): Option[String] = {
-    None
-  }
-}
+import pdi.jwt.{Jwt, JwtAlgorithm}
 
 case class Login(username: String, password: String)
+case class Register(username: String, password: String, email: String)
 object LoginJsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
   implicit val LoginFormats = jsonFormat2(Login)
+  implicit val RegisterFormats = jsonFormat3(Register)
 }
+
+object AuthManager {
+  private val secretKey = "PPSRiskala!"
+  private val jwtAlgorithm = JwtAlgorithm.HS256
+
+  private var credential: HashMap[String,String] = HashMap("Test"->"1234","Giordo"->"1234","NarcAle"->"1234")
+  def login(l: Login): Option[String] = {
+    credential get l.username flatMap(psw => if(psw == l.password) Some(genToken(l)) else None)
+  }
+
+  def register(r: Register): Option[String] = {
+    if(!credential.isDefinedAt(r.username)) {
+      credential = credential + (r.username -> r.password)
+      Some(genToken(Login(r.username,r.password)))
+    } else {
+      None
+    }
+  }
+
+  private def genToken(l: Login): String = {
+    import LoginJsonSupport._
+    import spray.json._
+    val claim = l.toJson.prettyPrint
+    println(claim)
+    Jwt.encode(claim, secretKey, jwtAlgorithm)
+  }
+}
+
