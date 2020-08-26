2e4d476ebed3aabddb793d5c7b6d2c3bde46b33a
Andrea <andrea.giordano5@studio.unibo.it>
2020-08-26 11:22:47 +0200

    Added account file

--- src/main/scala/org/riskala/controller/AuthManager.scala
+++ src/main/scala/org/riskala/controller/AuthManager.scala
@@ -1,10 +1,14 @@
 package org.riskala.controller
 
+import java.io.InputStream
+
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import org.riskala.Model.Account
 import spray.json.DefaultJsonProtocol
 
 import scala.collection.immutable.HashMap
 import pdi.jwt.{Jwt, JwtAlgorithm}
+import argonaut.Argonaut._
 
 case class Login(username: String, password: String)
 case class Register(username: String, password: String, email: String)
@@ -17,14 +21,20 @@ object AuthManager {
   private val secretKey = "PPSRiskala!"
   private val jwtAlgorithm = JwtAlgorithm.HS256
 
-  private var credential: HashMap[String,String] = HashMap("Test"->"1234","Giordo"->"1234","NarcAle"->"1234")
+  private val stream: InputStream = getClass.getResourceAsStream("/account.txt")
+  private val lines: String = scala.io.Source.fromInputStream( stream ).getLines.foldLeft("")(_+_)
+  private val accountList: List[Account] = lines.decodeOption[List[Account]].getOrElse(List.empty)
+
+  private var credential: HashMap[String,Account] = HashMap()
+  //HashMap("Test"->"1234","Giordo"->"1234","NarcAle"->"1234")
+  accountList.foreach(acc=> credential = credential+(acc.username->acc))
   def login(l: Login): Option[String] = {
-    credential get l.username flatMap(psw => if(psw == l.password) Some(genToken(l)) else None)
+    credential get l.username flatMap(acc => if(acc.password == l.password) Some(genToken(l)) else None)
   }
 
   def register(r: Register): Option[String] = {
     if(!credential.isDefinedAt(r.username)) {
-      credential = credential + (r.username -> r.password)
+      credential = credential + (r.username -> Account(r.username,r.password,r.email))
       Some(genToken(Login(r.username,r.password)))
     } else {
       None
