1b135c83e239bbd7ea516f894772b966886003ac
Andrea <andrea.giordano5@studio.unibo.it>
2020-09-02 01:00:42 +0200

    Refactor test (still not passed)

--- src/test/scala/org/riskala/Model/LobbyTest.scala
+++ src/test/scala/org/riskala/Model/LobbyTest.scala
@@ -1,98 +1,58 @@
 package org.riskala.Model
 
 import org.scalatest.wordspec.AnyWordSpec
-import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.testkit.typed.scaladsl.{ActorTestKit, TestProbe}
+import akka.actor.typed.ActorRef
 import org.riskala.Model.ModelMessages._
 import org.scalatest.BeforeAndAfterAll
 
 class LobbyTest extends AnyWordSpec with BeforeAndAfterAll {
-  val testKit = ActorTestKit()
+  val testKit: ActorTestKit = ActorTestKit()
 
   override def afterAll(): Unit = testKit.shutdownTestKit()
 
-  val lobby = testKit.spawn(LobbyManager(), "Lobby")
-  val probeCreate = testKit.createTestProbe[PlayerMessage]("probeCreate")
-  val probe2Create = testKit.createTestProbe[PlayerMessage]("probe2Create")
-  val probeJoin = testKit.createTestProbe[PlayerMessage]("probeJoin")
-  val probe2Join = testKit.createTestProbe[PlayerMessage]("probe2Join")
-  val probe3Join = testKit.createTestProbe[PlayerMessage]("probe3Join")
+  val lobby: ActorRef[LobbyMessage] = testKit.spawn(LobbyManager(), "Lobby")
+  val probeSub: TestProbe[PlayerMessage] = testKit.createTestProbe[PlayerMessage]("probeSub")
+  val probeCreate: TestProbe[PlayerMessage] = testKit.createTestProbe[PlayerMessage]("probeCreate")
+  val probeCreate2: TestProbe[PlayerMessage] = testKit.createTestProbe[PlayerMessage]("probeCreate2")
+  val probeJoin: TestProbe[PlayerMessage] = testKit.createTestProbe[PlayerMessage]("probeJoin")
+  val probeJoin2: TestProbe[PlayerMessage] = testKit.createTestProbe[PlayerMessage]("probeJoin2")
+  val probeJoin3: TestProbe[PlayerMessage] = testKit.createTestProbe[PlayerMessage]("probeJoin3")
 
-  "Lobby" should {
-    "accept subscriber" in {
-      val probe = testKit.createTestProbe[PlayerMessage]("probeSub")
-      lobby ! Subscribe(probe.ref)
-      probe.expectMessageType[PlayerMessage]
-      lobby ! Logout(probe.ref)
-    }
-  }
+  lobby ! Subscribe(probeSub.ref)
+  probeSub.expectMessageType[PlayerMessage]
+  lobby ! Logout(probeSub.ref)
+  probeSub.expectNoMessage()
 
-  "Lobby" should {
-    lobby ! Subscribe(probeCreate.ref)
-    lobby ! Subscribe(probe2Create.ref)
-    probeCreate.expectMessageType[PlayerMessage]
-    probe2Create.expectMessageType[PlayerMessage]
-    "accept creator" in {
-      lobby ! CreateRoom(probeCreate.ref, RoomInfo(RoomBasicInfo("Europa", 0, 4), ""))
-      probeCreate.expectNoMessage()
-      probe2Create.expectMessageType[PlayerMessage]
-      lobby ! Logout(probe2Create.ref)
-    }
-  }
+  lobby ! Subscribe(probeCreate.ref)
+  lobby ! Subscribe(probeCreate2.ref)
+  probeCreate.expectMessageType[PlayerMessage]
+  probeCreate2.expectMessageType[PlayerMessage]
+  lobby ! CreateRoom(probeCreate.ref, RoomInfo(RoomBasicInfo("Europa", 0, 4), ""))
+  probeCreate.expectNoMessage()
+  probeCreate2.expectMessageType[PlayerMessage]
+  lobby ! Logout(probeCreate2.ref)
 
-  "Lobby" should {
-    lobby ! Subscribe(probeJoin.ref)
-    lobby ! Subscribe(probe2Join.ref)
-    lobby ! Subscribe(probe3Join.ref)
-    probeJoin.expectMessageType[PlayerMessage]
-    probe2Join.expectMessageType[PlayerMessage]
-    probe3Join.expectMessageType[PlayerMessage]
+  println("FIN QUI TUTTO OK")
 
-    lobby ! CreateRoom(probeJoin.ref, RoomInfo(RoomBasicInfo("Usa", 0, 6), ""))
-    probeJoin.expectNoMessage()
-    probe2Join.expectMessageType[PlayerMessage]
-    probe3Join.expectMessageType[PlayerMessage]
-    "accept join to a room" in  {
-      lobby ! JoinTo(probe2Join.ref, "Usa")
-      probe2Join.expectNoMessage()
-    }
-  }
-/*
-  "Lobby" should {
+  lobby ! Subscribe(probeJoin.ref)
+  probeJoin.expectMessageType[PlayerMessage]
+  lobby ! Subscribe(probeJoin2.ref)
+  probeJoin2.expectMessageType[PlayerMessage]
+  lobby ! Subscribe(probeJoin3.ref)
+  probeJoin3.expectMessageType[PlayerMessage]
 
-    lobby ! Subscribe(probeJoin.ref)
-    probeJoin.expectMessageType[PlayerMessage]
-    lobby ! Subscribe(probe2Join.ref)
-    probe2Join.expectMessageType[PlayerMessage]
-
-    lobby ! Subscribe(probe3Join.ref)
-    probe3Join.expectMessageType[PlayerMessage]
-    lobby ! CreateRoom(probeJoin.ref, RoomInfo(RoomBasicInfo("Europa2", 0, 4), ""))
-    probeJoin.expectNoMessage()
-    probe2Join.expectMessageType[PlayerMessage]
-    probe3Join.expectMessageType[PlayerMessage]
-    "accept join to a room" in {
-      lobby ! JoinTo(probe2Join.ref, "Europa2")
-      probeJoin.expectNoMessage()
-    }
-    "not accept join to a room" in {
-      lobby ! JoinTo(probe3Join.ref, "USA")
-      probe3Join.expectMessageType[PlayerMessage]
-      lobby ! Logout(probe3Join.ref)
-    }
-  }
-*/
-  /*"Lobby" should {
-    val probe = testKit.createTestProbe[PlayerMessage]()
-    lobby ! Subscribe(probe.ref)
-    probe.expectMessageType[PlayerMessage]
-    lobby ! CreateRoom(probe.ref, RoomInfo(RoomBasicInfo("Europa2", 0, 4), ""))
-    probe.expectMessageType[PlayerMessage]
-    lobby ! JoinTo(probe.ref, "Europa2")
-    "start game" in {
-      lobby ! StartGame()
-    }
-  }*/
+  lobby ! CreateRoom(probeJoin.ref, RoomInfo(RoomBasicInfo("Usa", 0, 6), ""))
+  probeJoin.expectNoMessage()
+  //probeJoin2.expectMessageType[PlayerMessage]
+  //probeJoin3.expectMessageType[PlayerMessage]
 
 
+  lobby ! JoinTo(probeJoin2.ref, "Usa")
+  probeJoin2.expectNoMessage()
+
+  //probeJoin3.expectNoMessage()
+  lobby ! JoinTo(probeJoin3.ref, "America")
+  //probeJoin3.expectMessageType[PlayerMessage]
 
 }
