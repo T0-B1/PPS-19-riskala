ae5ca4fcd2a4386e402fb1ba51f75553f575744b
alessia <alessia.cerami@studio.unibo.it>
2020-09-01 16:44:09 +0200

    improved roomMessage

--- src/main/scala/org/riskala/Model/ModelMessages.scala
+++ src/main/scala/org/riskala/Model/ModelMessages.scala
@@ -1,12 +1,13 @@
 package org.riskala.Model
 
-import akka.actor.ActorRef
-
+import akka.actor.typed.ActorRef
 
 /**
  * Model messages
  */
 object ModelMessages {
+
+  trait PlayerMessage
 
   trait LobbyMessage
 
@@ -18,17 +19,25 @@ object ModelMessages {
    *  @param playerName     The player who is ready in a room
    *  @param actor          The reference of the player
    * */
-  case class Ready(playerName: String, actor: ActorRef) extends RoomMessage
+  case class Ready(playerName: String, actor: ActorRef[PlayerMessage])
+    extends RoomMessage
+
+  /** Message sent when an actor is not ready anymore
+   *  @param playerName     The player name who is not ready
+   *  @param actor          The reference of the player
+   * */
+  case class UnReady(playerName: String, actor: ActorRef[PlayerMessage])
+    extends RoomMessage
 
   /** Message sent when an actor wants to participate to a game
    * @param actor           The actor who want to participate to a game
    * */
-  case class Join(actor: ActorRef) extends GameMessage with RoomMessage
+  case class Join(actor: ActorRef[PlayerMessage]) extends GameMessage with RoomMessage
 
   /** Message sent when an actor wants to leave a game
    * @param actor           The actor who want to leave a game
    * */
-  case class Leave(actor: ActorRef) extends GameMessage with RoomMessage
+  case class Leave(actor: ActorRef[PlayerMessage]) extends GameMessage with RoomMessage
 
 
   sealed trait LogoutMessage extends LobbyMessage with GameMessage with RoomMessage
@@ -36,5 +45,5 @@ object ModelMessages {
   /** Message sent to exit
    * @param actor              The actor who wants to exit
    * */
-  case class Logout(actor: ActorRef) extends LogoutMessage
+  case class Logout(actor: ActorRef[PlayerMessage]) extends LogoutMessage
 }
