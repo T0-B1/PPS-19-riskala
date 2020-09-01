50ec1260476b9bb0f0cab828e3d69cad32fa06e0
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-01 05:25:28 +0200

    Minor

--- src/main/scala/org/riskala/controller/routes/SocketRoute.scala
+++ src/main/scala/org/riskala/controller/routes/SocketRoute.scala
@@ -4,6 +4,7 @@ import akka.Done
 import akka.actor.typed.ActorRef
 import akka.http.scaladsl.model.ws.Message
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import akka.stream.{CompletionStrategy, OverflowStrategy}
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.typed.scaladsl.ActorSource
@@ -12,16 +13,16 @@ import scala.concurrent.Future
 
 object SocketRoute {
 
-  val websocketRoute =
+  val websocketRoute: Route =
     (get & path("websocket") & parameter("token")) { token =>
       val authSink = sink(token)
       handleWebSocketMessages(Flow.fromSinkAndSource(authSink, source))
     }
 
-  val source: Source[Message, ActorRef[Message]] = ActorSource.actorRef[Message](completionMatcher = {
+  private val source: Source[Message, ActorRef[Message]] = ActorSource.actorRef[Message](completionMatcher = {
     case _ => CompletionStrategy.immediately
   }, PartialFunction.empty, bufferSize = 8, overflowStrategy = OverflowStrategy.fail)
 
-  def sink(sender: String): Sink[Message, Future[Done]] = Sink.foreach(m => println(s"Received $m from $sender"))
+  private def sink(sender: String): Sink[Message, Future[Done]] = Sink.foreach(m => println(s"Received $m from $sender"))
 
 }
