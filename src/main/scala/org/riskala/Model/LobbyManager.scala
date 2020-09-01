a218c1308bb3ca21690869ccb6c15f86e223a311
Andrea <andrea.giordano5@studio.unibo.it>
2020-09-02 01:01:40 +0200

    Refactored LobbyManager (still not passing tests)

--- src/main/scala/org/riskala/Model/LobbyManager.scala
+++ src/main/scala/org/riskala/Model/LobbyManager.scala
@@ -10,6 +10,7 @@ object LobbyManager {
 
   case class Lobby(rooms: List[String], games: List[String], terminatedGames: List[String])
 
+  /*
   private var subscribers: HashSet[ActorRef[PlayerMessage]] = HashSet.empty
   private var rooms: HashMap[String, (ActorRef[RoomMessage], RoomBasicInfo)] = HashMap.empty
   private var games: HashMap[String, ActorRef[GameMessage]] = HashMap.empty
@@ -23,79 +24,124 @@ object LobbyManager {
     val gameList: List[String] = games.keys.toList
     val terminatedGameList: List[String] = terminatedGames.keys.toList
     Lobby(roomList, gameList, terminatedGameList)
-
+    println("GETINFO")
     //TODO: return playerMessage
     new PlayerMessage {}
   }
 
   def notifyAllSubscribers(): Behavior[LobbyMessage] = {
-    subscribers.foreach(s => s ! getInfo())
+    subscribers.foreach(s => {
+      println("UPDATING " + s)
+      s ! getInfo()
+    })
+    println("NOTIFYALL")
     Behaviors.same
   }
-  
-  def apply(): Behavior[LobbyMessage] = Behaviors.receive { (context, message) =>
-    message match {
-      case Subscribe(subscriber) =>
-        subscribers = subscribers + subscriber
-        subscriber ! getInfo()
-        Behaviors.same
+  */
 
-      case CreateRoom(creator, roomInfo) =>
-        if (!rooms.contains(roomInfo.basicInfo.name)) {
-          //TODO: spawn RoomManager and send Join msg
-          val room = context.spawn(Behaviors.ignore[RoomMessage],"RoomManager")
-          //room ! Join(creator)
-          //TODO: swap null with room
-          rooms = rooms + (roomInfo.basicInfo.name -> (room, roomInfo.basicInfo))
-          subscribers = subscribers - creator
-          println(subscribers)
-        } else {
-          //TODO: Errore stanza già presente.
-          creator ! new PlayerMessage {}
-        }
-        notifyAllSubscribers()
+  def apply(): Behavior[LobbyMessage] = lobbyManager(HashSet.empty,HashMap.empty,HashMap.empty,HashMap.empty)
 
-      case JoinTo(actor, name) =>
-        println("received join message")
-        if (rooms.contains(name)) {
-          println("found room " + name)
-          subscribers = subscribers - actor
-          rooms.get(name).head._1 ! new RoomMessage {}
-        } else {
-          //TODO: Errore stanza non trovata.
-          println("No room found")
-          actor ! new PlayerMessage {}
-        }
-        println("join message done")
+  def lobbyManager(subscribers: HashSet[ActorRef[PlayerMessage]],
+                   rooms: HashMap[String, (ActorRef[RoomMessage], RoomBasicInfo)],
+                   games: HashMap[String, ActorRef[GameMessage]],
+                   terminatedGames: HashMap[String, (ActorRef[GameMessage], Boolean)]): Behavior[LobbyMessage] =
+    Behaviors.receive { (context, message) => {
 
-        Behaviors.same
+      def getInfo(): PlayerMessage = {
+        val roomList: List[String] = rooms.map(kv => kv._1 + " "
+          + kv._2._2.actualNumberOfPlayer
+          + " / " + kv._2._2.maxNumberOfPlayer).toList
+        val gameList: List[String] = games.keys.toList
+        val terminatedGameList: List[String] = terminatedGames.keys.toList
+        Lobby(roomList, gameList, terminatedGameList)
+        println("GETINFO")
+        //TODO: return playerMessage
+        new PlayerMessage {}
+      }
 
-      case StartGame(name, actor) =>
-        rooms = rooms - name
-        games = games + (name -> actor)
-        notifyAllSubscribers()
+      def notifyAllSubscribers(): Unit = {
+        subscribers.foreach(s => {
+          println("UPDATING " + s)
+          s ! getInfo()
+        })
+        println("NOTIFYALL")
+      }
 
-      case EndGame(name, game) =>
-        terminatedGames = terminatedGames + (name -> (game, true))
-        games = games - name
-        notifyAllSubscribers()
+      message match {
+        case Subscribe(subscriber) =>
+          val newSubs = subscribers + subscriber
+          subscriber ! getInfo()
+          println("SUB-MSG Sub:" + subscriber + " SubList:" + subscribers)
+          lobbyManager(subscribers = newSubs,rooms = rooms,games = games,terminatedGames = terminatedGames)
 
-      case GameClosed(name, subs) =>
-        terminatedGames = terminatedGames + (name -> (terminatedGames(name)._1, false))
-        subscribers = subscribers ++ subs
-        notifyAllSubscribers()
+        case CreateRoom(creator, roomInfo) =>
+          var newRooms: HashMap[String, (ActorRef[RoomMessage], RoomBasicInfo)] = rooms
+          var newSubs: HashSet[ActorRef[PlayerMessage]] = subscribers
+          if (!rooms.contains(roomInfo.basicInfo.name)) {
+            //TODO: spawn RoomManager and send Join msg
+            val room = context.spawn(Behaviors.ignore[RoomMessage], "RoomManager")
+            //room ! Join(creator)
+            //TODO: swap null with room
+            newRooms = rooms + (roomInfo.basicInfo.name -> (room, roomInfo.basicInfo))
+            newSubs = subscribers - creator
+            println("SUBS: " + subscribers)
+          } else {
+            //TODO: Errore stanza già presente.
+            creator ! new PlayerMessage {}
+          }
+          println("CREATE DONE, NOTIFYALL")
+          notifyAllSubscribers()
+          lobbyManager(subscribers = newSubs,rooms = newRooms,games = games,terminatedGames = terminatedGames)
 
-      case UpdateRoomInfo(info) =>
-        rooms = rooms + (info.name -> (rooms(info.name)._1, info))
-        notifyAllSubscribers()
+        case JoinTo(actor, name) =>
+          println("received join message")
+          var newSubs = subscribers
+          if (rooms.contains(name)) {
+            println("found room " + name)
+            newSubs = subscribers - actor
+            rooms.get(name).head._1 ! new RoomMessage {}
+          } else {
+            //TODO: Errore stanza non trovata.
+            println("No room found")
+            actor ! new PlayerMessage {}
+          }
+          println("join message done")
 
-      case EmptyRoom(roomName) =>
-        rooms = rooms - roomName
-        notifyAllSubscribers()
+          lobbyManager(subscribers = newSubs,rooms = rooms,games = games,terminatedGames = terminatedGames)
 
-      case Logout(actor) =>
-        subscribers = subscribers - actor
-        Behaviors.same
+        case StartGame(name, actor) =>
+          val newRooms = rooms - name
+          val newGames = games + (name -> actor)
+          notifyAllSubscribers()
+          lobbyManager(subscribers = subscribers,rooms = newRooms,games = newGames,terminatedGames = terminatedGames)
+
+        case EndGame(name, game) =>
+          val newTerminatedGames = terminatedGames + (name -> (game, true))
+          val newGames = games - name
+          notifyAllSubscribers()
+          lobbyManager(subscribers = subscribers,rooms = rooms,games = newGames,terminatedGames = newTerminatedGames)
+
+        case GameClosed(name, subs) =>
+          val newTerminatedGames = terminatedGames + (name -> (terminatedGames(name)._1, false))
+          val newSubs = subscribers ++ subs
+          notifyAllSubscribers()
+          lobbyManager(subscribers = newSubs,rooms = rooms,games = games,terminatedGames = newTerminatedGames)
+
+        case UpdateRoomInfo(info) =>
+          val newRooms = rooms + (info.name -> (rooms(info.name)._1, info))
+          notifyAllSubscribers()
+          lobbyManager(subscribers = subscribers,rooms = newRooms,games = games,terminatedGames = terminatedGames)
+
+        case EmptyRoom(roomName) =>
+          val newRooms = rooms - roomName
+          notifyAllSubscribers()
+          lobbyManager(subscribers = subscribers,rooms = newRooms,games = games,terminatedGames = terminatedGames)
+
+        case Logout(actor) =>
+          val newSubs = subscribers - actor
+          println("LOGOUT unSub:" + actor + " SubList:" + subscribers)
+          lobbyManager(subscribers = newSubs,rooms = rooms,games = games,terminatedGames = terminatedGames)
+      }
     }
   }
 
