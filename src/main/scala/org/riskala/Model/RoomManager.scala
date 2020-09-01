df3d929eb4f962f777d99d899a07dda25cfb8393
alessia <alessia.cerami@studio.unibo.it>
2020-09-01 16:45:09 +0200

    added RoomBehavior basic implementation

---
+++ src/main/scala/org/riskala/Model/RoomManager.scala
@@ -0,0 +1,28 @@
+package org.riskala.Model
+
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import org.riskala.Model.ModelMessages._
+
+import scala.collection.immutable.HashMap
+
+object RoomManager {
+  var subscribersRoom: Set[ActorRef[_]] = Set.empty
+  var readyPlayerList: HashMap[String,ActorRef[_]] = HashMap.empty
+
+  def apply(): Behavior[RoomMessage] = {
+    Behaviors.receive { (context, message) =>
+      message match {
+
+        case Join(actor) => ???
+
+        case Leave(actor) => ???
+
+        case UnReady(playerName, actor) => ???
+
+        case Ready(playerName, actor) => ???
+
+      }
+    }
+  }
+}
