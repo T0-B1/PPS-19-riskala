175b28f203eaf03f8a2b8050ca643e5f55166f9b
alessia <alessia.cerami@studio.unibo.it>
2020-09-02 10:16:06 +0200

    implemented leave message + added lobby reference

--- src/main/scala/org/riskala/Model/Room/RoomManager.scala
+++ src/main/scala/org/riskala/Model/Room/RoomManager.scala
@@ -2,7 +2,7 @@ package org.riskala.Model.Room
 
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
-import RoomMessages._
+import RoomMessages.{LobbyMessage, _}
 
 import scala.collection.immutable.{HashMap, HashSet}
 
@@ -16,14 +16,15 @@ object RoomManager {
   //TODO: to remove
   case class RoomInfo(basicInfo: RoomBasicInfo, scenario: String)
 
-  def apply(roomInfo: RoomInfo): Behavior[RoomMessage] = {
+  def apply(roomInfo: RoomInfo, lobby: ActorRef[LobbyMessage]): Behavior[RoomMessage] = {
 
-    roomManager(HashSet.empty, HashMap.empty, roomInfo)
+    roomManager(HashSet.empty, HashMap.empty, roomInfo, lobby)
   }
 
   def roomManager(subscribersRoom: HashSet[ActorRef[PlayerMessage]],
                   readyPlayerList: HashMap[String,ActorRef[PlayerMessage]],
-                  roomInfo: RoomInfo
+                  roomInfo: RoomInfo,
+                  lobby: ActorRef[LobbyMessage]
                  ):Behavior[RoomMessage] = {
 
     Behaviors.receive { (context, message) =>
@@ -33,9 +34,21 @@ object RoomManager {
           val newSubscriber = subscribersRoom + actor
           //TODO: change into info
           actor ! new PlayerMessage {}
-          roomManager(newSubscriber, readyPlayerList, roomInfo)
+          roomManager(newSubscriber, readyPlayerList, roomInfo, lobby)
 
-        case Leave(actor) => ???
+        case Leave(actor) =>
+          //Remove the actor from subscribersList
+          val newSubscriber = subscribersRoom - actor
+          if (newSubscriber.isEmpty && readyPlayerList.isEmpty) {
+            /*If there is any player:
+             (1) send message to lobby (emptyRoom);
+             (2)Behaviors.stopped
+             */
+            //TODO: EmptyRoom(info.basicInfo.name)
+            lobby ! new LobbyMessage {}
+            Behaviors.stopped
+          }
+          roomManager(newSubscriber, readyPlayerList, roomInfo, lobby)
 
         case UnReady(playerName, actor) => ???
 
