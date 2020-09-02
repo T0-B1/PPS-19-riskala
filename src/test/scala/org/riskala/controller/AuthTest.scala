080d45a0063a60a1a0153f8a0d014e8bef2446ed
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-02 22:16:43 +0200

    Login testing

--- src/test/scala/org/riskala/controller/AuthTest.scala
+++ src/test/scala/org/riskala/controller/AuthTest.scala
@@ -2,21 +2,23 @@ package org.riskala.controller
 
 import java.io.FileNotFoundException
 import java.util.Properties
-
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.junit.runner.RunWith
-import org.scalatest.{BeforeAndAfter, Matchers}
+import org.scalatest.BeforeAndAfter
+import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.junit.JUnitRunner
 
 import scala.io.Source
 
 @RunWith(classOf[JUnitRunner])
-class AuthTest extends AnyWordSpec with BeforeAndAfter with ScalatestRouteTest {
+class AuthTest extends AnyWordSpec with Matchers with BeforeAndAfter with ScalatestRouteTest {
 
   val properties: Properties = new Properties()
 
   before{
+    println("BEFORE EXECUTED")
     val path = "/test.properties"
     val url = getClass.getResource(path)
     if (url != null)
@@ -25,14 +27,17 @@ class AuthTest extends AnyWordSpec with BeforeAndAfter with ScalatestRouteTest {
       throw new FileNotFoundException(s"Properties file at path $path cannot be loaded");
   }
 
-  properties.get("testAccountUsername")
-  properties.get("testAccountPassword")
-  properties.get("testAccountEmail")
+  def login(username: String, password: String): String = {
+    Post("/login", HttpEntity(ContentTypes.`application/json`, s"""{"username":"$username","password":"$password"}""")) ~> RouteManager.allRoutes ~> check {
+      response.status shouldEqual StatusCodes.OK
+      responseAs[String]
+    }
+  }
 
   "A user" when {
     "registered" should {
       "be able to login" in {
-
+        login(properties.get("testAccountUsername").toString, properties.get("testAccountPassword").toString)
       }
       "not be able to register again" in {
 
