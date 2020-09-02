8d673916c6234034cc9f5f066df5ae614508d622
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-02 18:45:38 +0200

    Auth test

---
+++ src/test/scala/org/riskala/controller/AuthTest.scala
@@ -0,0 +1,29 @@
+package org.riskala.controller
+
+import org.junit.runner.RunWith
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class AuthTest extends AnyWordSpec {
+
+  "A user" when {
+    "registered" should {
+      "be able to login" in {
+
+      }
+      "not be able to register again" in {
+
+      }
+    }
+    "logged" should {
+      "be able to verify his token" in {
+
+      }
+      "be able to retrieve his username from the token" in {
+
+      }
+    }
+  }
+
+}
