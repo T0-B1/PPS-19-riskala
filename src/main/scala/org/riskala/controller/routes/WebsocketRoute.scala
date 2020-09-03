f739f99db6695f0874ec8998ceb12ce374099f54
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-04 00:53:02 +0200

    Rejecting unauthorized socket requests

--- src/main/scala/org/riskala/controller/routes/WebsocketRoute.scala
+++ src/main/scala/org/riskala/controller/routes/WebsocketRoute.scala
@@ -2,12 +2,14 @@ package org.riskala.controller.routes
 
 import akka.Done
 import akka.actor.typed.ActorRef
+import akka.http.scaladsl.model.{StatusCode, StatusCodes}
 import akka.http.scaladsl.model.ws.Message
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.{CompletionStrategy, OverflowStrategy}
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.typed.scaladsl.ActorSource
+import org.riskala.controller.AuthManager
 
 import scala.concurrent.Future
 
@@ -15,8 +17,11 @@ object WebsocketRoute {
 
   val websocketRoute: Route =
     (get & path("websocket") & parameter("token")) { token =>
-      val authSink = sink(token)
-      handleWebSocketMessages(Flow.fromSinkAndSource(authSink, source))
+      if(!AuthManager.checkToken(token)) complete(StatusCodes.Forbidden)
+      else{
+        val authSink = sink(token)
+        handleWebSocketMessages(Flow.fromSinkAndSource(authSink, source))
+      }
     }
 
   private val source: Source[Message, ActorRef[Message]] = ActorSource.actorRef[Message](completionMatcher = {
