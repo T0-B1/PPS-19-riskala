0c966de7be9e0b16b5e4a797e61fde7d3d74696b
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-03 20:46:36 +0200

    Moved login a register testing facilities in companion object

--- src/test/scala/org/riskala/controller/AuthTest.scala
+++ src/test/scala/org/riskala/controller/AuthTest.scala
@@ -1,10 +1,11 @@
 package org.riskala.controller
 
 import java.util.Properties
+
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.junit.runner.RunWith
-import org.scalatest.{Assertion, BeforeAndAfter}
+import org.scalatest.Assertion
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.junit.JUnitRunner
@@ -14,11 +15,37 @@ import org.riskala.utils
 class AuthTest extends AnyWordSpec with Matchers with ScalatestRouteTest {
 
   val properties: Properties = utils.loadPropertiesFromResources()
-  val server = Server()
+  val server: Server = Server()
+
+  "A user" when {
+    "registered" should {
+      "be able to login" in {
+        AuthTest.login(properties.get("testAccountUsername").toString, properties.get("testAccountPassword").toString)
+      }
+      "not be able to register again" in {
+        AuthTest.register(properties.get("testAccountUsername").toString, properties.get("testAccountPassword").toString, properties.get("testAccountPassword").toString)
+      }
+    }
+    "logged" should {
+      "be able to verify his token" in {
+        val token = AuthTest.login(properties.get("testAccountUsername").toString, properties.get("testAccountPassword").toString)
+        AuthManager.checkToken(token) shouldBe true
+      }
+      "be able to retrieve his username from the token" in {
+        val username = properties.get("testAccountUsername").toString
+        val token = AuthTest.login(username, properties.get("testAccountPassword").toString)
+        Some(username) shouldBe AuthManager.getUser(token)
+      }
+    }
+  }
+
+}
+
+object AuthTest extends AnyWordSpec with Matchers  with ScalatestRouteTest{
 
   def login(username: String, password: String): String = {
     val json: String = LoginJsonSupport.LoginFormats.write(Login(username, password)).toString()
-    Post("/login", HttpEntity(ContentTypes.`application/json`, json)) ~> server.routing ~> check {
+    Post("/login", HttpEntity(ContentTypes.`application/json`, json)) ~> Server.routing ~> check {
       response.status shouldEqual StatusCodes.OK
       responseAs[String]
     }
@@ -26,31 +53,8 @@ class AuthTest extends AnyWordSpec with Matchers with ScalatestRouteTest {
 
   def register(username: String, password: String, email: String): Assertion = {
     val json: String = LoginJsonSupport.RegisterFormats.write(Register(username, password, email)).toString()
-    Post("/register", HttpEntity(ContentTypes.`application/json`, json)) ~> server.routing ~> check {
+    Post("/register", HttpEntity(ContentTypes.`application/json`, json)) ~> Server.routing ~> check {
       response.status shouldEqual StatusCodes.BadRequest
     }
   }
-
-  "A user" when {
-    "registered" should {
-      "be able to login" in {
-        login(properties.get("testAccountUsername").toString, properties.get("testAccountPassword").toString)
-      }
-      "not be able to register again" in {
-        register(properties.get("testAccountUsername").toString, properties.get("testAccountPassword").toString, properties.get("testAccountPassword").toString)
-      }
-    }
-    "logged" should {
-      "be able to verify his token" in {
-        val token = login(properties.get("testAccountUsername").toString, properties.get("testAccountPassword").toString)
-        AuthManager.checkToken(token) shouldBe true
-      }
-      "be able to retrieve his username from the token" in {
-        val username = properties.get("testAccountUsername").toString
-        val token = login(username, properties.get("testAccountPassword").toString)
-        Some(username) shouldBe AuthManager.getUser(token)
-      }
-    }
-  }
-
 }
