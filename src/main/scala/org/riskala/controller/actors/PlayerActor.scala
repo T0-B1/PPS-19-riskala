3c71925806e9045a662e104d88da649aa9742e9c
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-05 06:13:55 +0200

    Removed socket ref to player actor (will not be available at spawn time)

--- src/main/scala/org/riskala/controller/actors/PlayerActor.scala
+++ src/main/scala/org/riskala/controller/actors/PlayerActor.scala
@@ -3,20 +3,20 @@ package org.riskala.controller.actors
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
-import org.riskala.controller.actors.PlayerMessages._
+import org.riskala.controller.actors.ServerMessages._
 
 object PlayerActor {
 
-  def apply(username: String, socket: ActorRef[Message]): Behavior[PlayerMessage] = {
-    playerActor(username, socket)
+  def apply(username: String): Behavior[PlayerMessage] = {
+    playerActor(username)
   }
 
-  private def playerActor(username: String, socket: ActorRef[Message]): Behavior[PlayerMessage] =
+  private def playerActor(username: String): Behavior[PlayerMessage] =
     Behaviors.receive { (context, message) =>
       message match {
         case SocketMessage(payload) => {
           context.log.debug(s"PlayerActor of $username received socket payload: $payload")
-          socket ! TextMessage(s"PlayerActor of $username echoing: $payload")
+          //socket ! TextMessage(s"PlayerActor of $username echoing: $payload")
         }
       }
       Behaviors.same
