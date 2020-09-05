15c220ea9261d1d389c6f82000469fc07eae0386
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-05 19:03:33 +0200

    Binding websocket to playerActor

--- src/main/scala/org/riskala/controller/routes/WebsocketRoute.scala
+++ src/main/scala/org/riskala/controller/routes/WebsocketRoute.scala
@@ -12,7 +12,8 @@ import akka.stream.typed.scaladsl.ActorSource
 import org.riskala.controller.AuthManager
 import org.riskala.controller.Server
 import org.riskala.controller.actors.PlayerActor
-import org.riskala.controller.actors.ServerMessages.PlayerMessage
+import org.riskala.controller.actors.ServerMessages.{PlayerMessage, ServerMessage, SocketMessage}
+import org.riskala.view.SocketActor.SocketMessage
 
 import scala.concurrent.Future
 
@@ -22,29 +23,24 @@ object WebsocketRoute {
 
   val websocketRoute: Route =
     (get & path("websocket") & parameter("token")) { token =>
-      if(!AuthManager.checkToken(token)) complete(StatusCodes.Forbidden)
+      if( (!AuthManager.checkToken(token)) || AuthManager.getUser(token).isEmpty) complete(StatusCodes.Forbidden)
       else{
-        val authSink = sink(token)
-
-        val (wsActor, wsSource) =  Source.actorRef[Message](
-          completionMatcher = {
-            case Done =>
-              // complete stream immediately if we send it Done
-              CompletionStrategy.immediately
-          },
-          // never fail the stream because of a message
-          failureMatcher = PartialFunction.empty,
-          bufferSize = 100,
-          overflowStrategy = OverflowStrategy.dropHead).preMaterialize()
-
-        handleWebSocketMessages(Flow.fromSinkAndSource(authSink, source))
+        val username = AuthManager.getUser(token).get
+        println(s"Websocket created for player $username")
+        val (wsActor, wsSource) = source.preMaterialize()
+        val playerActorRef: ActorRef[PlayerMessage] = system.systemActorOf(PlayerActor(username), username)
+        val wsSink = sink(playerActorRef)
+        // TODO use spawn protocol
+        // https://doc.akka.io/docs/akka/current/typed/actor-lifecycle.html#spawnprotocol
+        handleWebSocketMessages(Flow.fromSinkAndSource(wsSink, wsSource))
       }
     }
 
   private val source: Source[Message, ActorRef[Message]] = ActorSource.actorRef[Message](completionMatcher = {
     case _ => CompletionStrategy.immediately
-  }, PartialFunction.empty, bufferSize = 8, overflowStrategy = OverflowStrategy.fail)
+  }, PartialFunction.empty, bufferSize = 8, overflowStrategy = OverflowStrategy.dropHead)
 
-  private def sink(sender: String): Sink[Message, Future[Done]] = Sink.foreach(m => println(s"Received $m from $sender"))
+  private def sink(playerRef: ActorRef[PlayerMessage]): Sink[Message, Future[Done]] =
+    Sink.foreach(m => playerRef ! SocketMessage(m.asTextMessage.getStrictText))
 
 }
