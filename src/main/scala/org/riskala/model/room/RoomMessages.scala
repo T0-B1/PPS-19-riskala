572627534c40deecafd880f99bf6ad52f04fac1c
alessia <alessia.cerami@studio.unibo.it>
2020-09-06 15:34:48 +0200

    refactor

--- src/main/scala/org/riskala/model/room/RoomMessages.scala
+++ src/main/scala/org/riskala/model/room/RoomMessages.scala
@@ -10,22 +10,22 @@ object RoomMessages {
 
   /**
    * @param name                    Room name
-   * @param actualNumberOfPlayer    Number of player in a room
-   * @param maxNumberOfPlayer       Maximun numbere of player of a room
+   * @param actualNumberOfPlayer    Number of players in a room
+   * @param maxNumberOfPlayer       Maximum number of players in a room
    * */
   case class RoomBasicInfo(name: String,
                            actualNumberOfPlayer: Int,
                            maxNumberOfPlayer: Int)
 
   /**
-   * @param basicInfo               Object containing the basic information of a room
+   * @param basicInfo               Object containing basic information of a room
    * @param scenario                Name of the game map
    * */
   case class RoomInfo(basicInfo: RoomBasicInfo, scenario: String)
 
   /** Message sent when an actor is ready in a room
-   *  @param playerName     The player who is ready in a room
-   *  @param actor          The reference of the player
+   *  @param playerName     The name of the player who is ready in a room
+   *  @param actor          The player ready in  a room
    * */
   case class Ready(playerName: String, actor: ActorRef[PlayerMessage]) extends RoomMessage
 
@@ -36,12 +36,12 @@ object RoomMessages {
   case class UnReady(playerName: String, actor: ActorRef[PlayerMessage]) extends RoomMessage
 
   /** Message sent when an actor wants to participate to a game
-   * @param actor           The actor who want to participate to a game
+   * @param actor           The actor who wants to participate to a game
    * */
   case class Join(actor: ActorRef[PlayerMessage]) extends GameMessage with RoomMessage
 
   /** Message sent when an actor wants to leave a game
-   * @param actor           The actor who want to leave a game
+   * @param actor           The actor who wants to leave a game
    * */
   case class Leave(actor: ActorRef[PlayerMessage]) extends GameMessage with RoomMessage
 
