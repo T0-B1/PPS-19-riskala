c5675cbcfaab7e35efdd7137a12868818dbe42c0
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-06 02:06:53 +0200

    Msg to register socket to PlayerActor

--- src/main/scala/org/riskala/controller/actors/ServerMessages.scala
+++ src/main/scala/org/riskala/controller/actors/ServerMessages.scala
@@ -1,5 +1,8 @@
 package org.riskala.controller.actors
 
+import akka.actor.typed.ActorRef
+import akka.http.scaladsl.model.ws.Message
+
 object ServerMessages {
 
   trait ServerMessage
@@ -7,5 +10,7 @@ object ServerMessages {
   trait PlayerMessage extends ServerMessage
 
   final case class SocketMessage(payload: String) extends PlayerMessage
+
+  final case class RegisterSocket(socketActor: ActorRef[Message]) extends PlayerMessage
 
 }
