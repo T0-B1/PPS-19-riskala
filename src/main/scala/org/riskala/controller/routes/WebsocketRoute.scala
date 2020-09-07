2111856a3f73e9b2bb375146b766affd0316b392
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-07 03:04:48 +0200

    Fixed connection issues in websocket

--- src/main/scala/org/riskala/controller/routes/WebsocketRoute.scala
+++ src/main/scala/org/riskala/controller/routes/WebsocketRoute.scala
@@ -1,18 +1,17 @@
 package org.riskala.controller.routes
 
-import akka.Done
-import akka.actor.typed.{ActorRef, Props}
-import akka.http.scaladsl.model.{StatusCode, StatusCodes}
+import akka.actor.typed.ActorRef
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.ws.Message
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import akka.stream.{CompletionStrategy, OverflowStrategy}
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.typed.scaladsl.ActorSource
-import org.riskala.controller.AuthManager
-import org.riskala.controller.Server
+import akka.stream.{CompletionStrategy, OverflowStrategy}
+import akka.{Done, actor}
 import org.riskala.controller.actors.PlayerActor
-import org.riskala.controller.actors.ServerMessages.{PlayerMessage, RegisterSocket, SocketMessage}
+import org.riskala.controller.actors.ServerMessages.{PlayerMessage, SocketMessage}
+import org.riskala.controller.{AuthManager, Server}
 
 import scala.concurrent.Future
 
@@ -26,18 +25,20 @@ object WebsocketRoute {
       else{
         val username = AuthManager.getUser(token).get
         system.log.info(s"Websocket created for player $username")
-        val (wsActor, wsSource) = source.preMaterialize()
+        val (wsActor, wsSource) = untypedActorSource().preMaterialize()
         // TODO use spawn protocol
         // https://doc.akka.io/docs/akka/current/typed/actor-lifecycle.html#spawnprotocol
-        val playerActorRef: ActorRef[PlayerMessage] = system.systemActorOf(PlayerActor(username), username)
-        playerActorRef ! RegisterSocket(wsActor)
+        val playerActorRef: ActorRef[PlayerMessage] = system.systemActorOf(PlayerActor(username, wsActor), username)
         handleWebSocketMessages(Flow.fromSinkAndSource(sinkFromActor(playerActorRef), wsSource))
       }
     }
 
-  private val source: Source[Message, ActorRef[Message]] = ActorSource.actorRef[Message](completionMatcher = {
+  private def typedActorSource(): Source[Message, ActorRef[Message]] = ActorSource.actorRef[Message](completionMatcher = {
     case _ => CompletionStrategy.immediately
   }, PartialFunction.empty, bufferSize = 8, overflowStrategy = OverflowStrategy.dropHead)
+
+  private def untypedActorSource(): Source[Message, actor.ActorRef] =
+    Source.actorRef[Message](bufferSize = 8, overflowStrategy = OverflowStrategy.dropHead)
 
   private def sinkFromActor(playerRef: ActorRef[PlayerMessage]): Sink[Message, Future[Done]] =
     Sink.foreach(m => playerRef ! SocketMessage(m.asTextMessage.getStrictText))
