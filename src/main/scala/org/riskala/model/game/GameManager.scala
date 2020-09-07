4e74f6d36ef5394ae6745a9c92c8fd59c8493207
alessia <alessia.cerami@studio.unibo.it>
2020-09-07 16:41:52 +0200

    Basic GameManager implementation

--- src/main/scala/org/riskala/model/game/GameManager.scala
+++ src/main/scala/org/riskala/model/game/GameManager.scala
@@ -4,15 +4,54 @@ import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 import org.riskala.controller.actors.PlayerMessages.PlayerMessage
 import org.riskala.model.ModelMessages.GameMessage
+import org.riskala.model.game.GameMessages._
+import org.riskala.model.room.RoomMessages.RoomInfo
 
-import scala.collection.immutable.HashSet
+import scala.collection.immutable.{HashMap, HashSet}
 
 object GameManager {
-  def apply(): Behavior[GameMessage] = nextBehavior(HashSet.empty)
+  def apply(roomInfo: RoomInfo, subscribers: HashSet[ActorRef[PlayerMessage]],
+            players: HashMap[String,ActorRef[PlayerMessage]]): Behavior[GameMessage] =
+    gameManager(roomInfo, HashSet.empty, HashMap.empty)
 
-  private def nextBehavior(subscribers: HashSet[ActorRef[PlayerMessage]]): Behavior[GameMessage] =
+  private def gameManager(newRoomInfo: RoomInfo,
+                           newSubscribers: HashSet[ActorRef[PlayerMessage]],
+                           newPlayers: HashMap[String,ActorRef[PlayerMessage]]): Behavior[GameMessage] =
     Behaviors.receive { (context,message) => {
-      Behaviors.ignore[GameMessage]
+
+      def nextBehavior(nextRoomInfo: RoomInfo = newRoomInfo,
+                       nextSubscribers: HashSet[ActorRef[PlayerMessage]] = newSubscribers,
+                       nextPlayers: HashMap[String,ActorRef[PlayerMessage]] = newPlayers
+                      ): Behavior[GameMessage] = gameManager(nextRoomInfo, nextSubscribers,nextPlayers)
+      message match {
+        case Join() =>
+          context.log.info("Join")
+          nextBehavior()
+
+        case Leave() =>
+          context.log.info("Leave")
+          nextBehavior()
+
+        case Deploy() =>
+          context.log.info("Deploy")
+          nextBehavior()
+
+        case Attack() =>
+          context.log.info("Attack")
+          nextBehavior()
+
+        case Move() =>
+          context.log.info("Move")
+          nextBehavior()
+
+        case RedeemBonus() =>
+          context.log.info("RedeemBonus")
+          nextBehavior()
+
+        case EndTurn() =>
+          context.log.info("EndTurn")
+          nextBehavior()
+      }
     }
   }
 }
