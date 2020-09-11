a8290ee8088920c41592ff6ec7c69d7e138fcfd4
Andrea <andrea.giordano5@studio.unibo.it>
2020-09-11 02:05:26 +0200

    Parser wrap

--- src/main/scala/org/riskala/utils/Parser.scala
+++ src/main/scala/org/riskala/utils/Parser.scala
@@ -2,11 +2,15 @@ package org.riskala.utils
 
 import org.riskala.controller.actors.PlayerMessages.WrappedMessage
 import argonaut.Argonaut._
-import argonaut.DecodeJson
+import argonaut.{DecodeJson, EncodeJson}
 
 object Parser {
 
   case class TypedMessage(classType: Class[_], payload: String)
+
+  def wrap[T](className: String, payload: T, encoder: EncodeJson[T]): String = {
+    WrappedMessage(className,payload.asJson(encoder).pretty(spaces2)).asJson.pretty(spaces2)
+  }
 
   def unwrap(message: String): TypedMessage = {
     val parsedMsg: WrappedMessage = retrieveWrapped(message).get
