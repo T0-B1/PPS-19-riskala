4932e377f344336df26877f19e2a5ab9cf49f663
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-11 13:23:54 +0200

    Fixed server to spawn a generic PlayerActor

--- src/main/scala/org/riskala/controller/routes/WebsocketRoute.scala
+++ src/main/scala/org/riskala/controller/routes/WebsocketRoute.scala
@@ -10,7 +10,7 @@ import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.typed.scaladsl.ActorSource
 import akka.stream.{CompletionStrategy, OverflowStrategy}
 import akka.{Done, NotUsed, actor}
-import org.riskala.controller.actors.{PlayerLobbyBehavior, PlayerMessages}
+import org.riskala.controller.actors.{PlayerActor, PlayerLobbyBehavior, PlayerMessages}
 import org.riskala.controller.actors.PlayerMessages.PlayerMessage
 import org.riskala.controller.{AuthManager, Server}
 import org.riskala.utils.Utils
@@ -42,7 +42,7 @@ object WebsocketRoute {
     Utils.askReceptionistToFind[PlayerMessage](username).toSeq match {
       // No PlayerActor registered found
       case Seq() => {
-        val ref: ActorRef[PlayerMessage] = system.systemActorOf(PlayerLobbyBehavior(username, socket = newSocket), username)
+        val ref: ActorRef[PlayerMessage] = system.systemActorOf(PlayerActor(username, newSocket), username)
         system.receptionist ! Receptionist.Register(ServiceKey[PlayerMessage](username), ref)
         ref
       }
