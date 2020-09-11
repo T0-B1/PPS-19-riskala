e5d64c88f63cda1f8954aa57be9b092587e0d028
Andrea <andrea.giordano5@studio.unibo.it>
2020-09-11 02:12:11 +0200

    Consume FromClient message

--- src/main/scala/org/riskala/controller/actors/PlayerLobbyBehavior.scala
+++ src/main/scala/org/riskala/controller/actors/PlayerLobbyBehavior.scala
@@ -5,58 +5,63 @@ import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import org.riskala.controller.actors.PlayerMessages._
-import org.riskala.model.ModelMessages.LobbyMessage
-import org.riskala.model.lobby.LobbyMessages.JoinTo
+import org.riskala.model.ModelMessages.{LobbyMessage, Logout}
+import org.riskala.model.lobby.LobbyMessages.{CreateRoom, JoinTo, LobbyInfo}
+import org.riskala.model.room.RoomMessages.{RoomBasicInfo, RoomInfo}
 import org.riskala.utils.Parser
+import argonaut.Argonaut._
 
 object PlayerLobbyBehavior {
 
-  def apply(username: String, socket: actor.ActorRef): Behavior[PlayerMessage] = {
-    playerActor(username, socket)
+  def apply(username: String, lobby: ActorRef[LobbyMessage] = null, socket: actor.ActorRef): Behavior[PlayerMessage] = {
+    playerActor(username, lobby, socket)
   }
 
-  private def playerActor(username: String, socket: actor.ActorRef): Behavior[PlayerMessage] =
+  private def playerActor(username: String,
+                          lobby: ActorRef[LobbyMessage],
+                          socket: actor.ActorRef): Behavior[PlayerMessage] =
     Behaviors.receive { (context, message) =>
 
-      def nextBehavior(newUsername: String = username, newSocket: actor.ActorRef = socket): Behavior[PlayerMessage] =
-        playerActor(newUsername, newSocket)
+      def nextBehavior(newUsername: String = username,
+                       newLobby: ActorRef[LobbyMessage] = lobby,
+                       newSocket: actor.ActorRef = socket): Behavior[PlayerMessage] =
+        playerActor(newUsername, newLobby, newSocket)
 
       message match {
-        case SocketMessage(payload) => {
+        case SocketMessage(payload) =>
           context.log.info(s"PlayerActor of $username received socket payload: $payload")
-          socket ! TextMessage(s"PlayerActor of $username echoing: $payload")
-
-          val referent: ActorRef[LobbyMessage] = ???
           val typed = Parser.unwrap(payload)
           typed.classType match {
-            case _: Class[JoinMessage] => {
+            case _: Class[JoinMessage] =>
               Parser.retrieveMessage(typed.payload, JoinMessage.JoinCodecJson.Decoder)
-                .foreach(j => referent ! JoinTo(context.self,j.name))
-            }
+                .foreach(j => lobby ! JoinTo(context.self,j.name))
+              nextBehavior()
+            case _: Class[CreateMessage] =>
+              Parser.retrieveMessage(typed.payload, CreateMessage.CreateCodecJson.Decoder)
+                .foreach(r =>
+                  lobby ! CreateRoom(context.self, RoomInfo(RoomBasicInfo(r.name,0,r.maxPlayer), r.scenario)))
+              nextBehavior()
+            case _: Class[LogoutMessage] =>
+              lobby ! Logout(context.self)
+              //TODO: close socket
+              Behaviors.stopped
           }
 
-          Parser.retrieveWrapped(payload).foreach(wrapped=>wrapped.classType match {
-            case "ErrorMessage" =>
-              val opt = Parser.retrieveMessage[ErrorMessage](wrapped.payload,ErrorMessage.ErrorCodecJson.Decoder)
-
-          })
+        case LobbyInfoMessage(lobbyInfo) =>
+          context.log.info(s"PlayerActor of $username received LobbyInfoMessage")
+          socket ! TextMessage(Parser.wrap("LobbyInfo",lobbyInfo,LobbyInfo.LobbyInfoCodecJson.Encoder))
           nextBehavior()
-        }
-        case RoomInfoMessage(roomInfo) => context.log.info("RoomInfoMessage"); nextBehavior()
-        case LobbyInfoMessage(lobbyInfo) => context.log.info("LobbyInfoMessage"); nextBehavior()
-        case GameInfoMessage() => context.log.info("GameInfoMessage"); nextBehavior()
-        case JoinMessage(name) => context.log.info("JoinMessage"); nextBehavior()
-        case ReadyMessage() => context.log.info("ReadyMessage"); nextBehavior()
-        case CreateMessage(name,maxPlayer,scenario) => context.log.info("CreateMessage"); nextBehavior()
-        case LeaveMessage() => context.log.info("LeaveMessage"); nextBehavior()
-        case ActionMessage(from,to,attacking,defending,invading) => context.log.info("ActionMessage"); nextBehavior()
-        case RedeemBonusMessage(cardType) => context.log.info("RedeemBonusMessage"); nextBehavior()
-        case EndTurnMessage() => context.log.info("EndTurnMessage"); nextBehavior()
-        case ErrorMessage(error) => context.log.info("ErrorMessage"); nextBehavior()
-        case RegisterSocket(newSocketActor) => {
+        case error: ErrorMessage =>
+          context.log.info(s"PlayerActor of $username received ErrorMessage")
+          socket ! TextMessage(Parser.wrap("ErrorMessage",error, ErrorMessage.ErrorCodecJson.Encoder))
+          nextBehavior()
+        case RegisterSocket(newSocketActor) =>
           context.log.info("registering new socket")
           nextBehavior(newSocket = newSocketActor)
-        }
+
+        case x =>
+          context.log.info(s"PlayerActor of $username received "+ x +", IGNORED")
+          nextBehavior()
       }
     }
 
