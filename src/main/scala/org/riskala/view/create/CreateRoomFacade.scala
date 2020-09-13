53b0598b489a728d311ac6716e2b1d693ade320b
alessia <alessia.cerami@studio.unibo.it>
2020-09-13 16:34:39 +0200

    facade clientRoom

--- src/main/scala/org/riskala/view/create/CreateRoomFacade.scala
+++ src/main/scala/org/riskala/view/create/CreateRoomFacade.scala
@@ -7,4 +7,5 @@ import scala.scalajs.js.annotation.JSGlobal
 @JSGlobal
 class CreateRoomFacade extends js.Object {
   def notifyCreateError(error: String): Unit = js.native
+  def goToRoom(roomInfo: String): Unit = js.native
 }
