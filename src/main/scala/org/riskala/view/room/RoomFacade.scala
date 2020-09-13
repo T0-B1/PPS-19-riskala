00aae5a8f28b16af5b77ee620eb053605ba063fc
alessia <alessia.cerami@studio.unibo.it>
2020-09-13 18:51:29 +0200

    updated room facade

--- src/main/scala/org/riskala/view/room/RoomFacade.scala
+++ src/main/scala/org/riskala/view/room/RoomFacade.scala
@@ -7,4 +7,5 @@ import scala.scalajs.js.annotation.JSGlobal
 class RoomFacade extends js.Object{
   def addPlayers(name:String): Unit = js.native
   def clearPlayer(): Unit = js.native
+  def setName(roomName: String): Unit = js.native
 }
