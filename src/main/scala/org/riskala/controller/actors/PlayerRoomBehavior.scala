14a160cf1ce9d188ba06ea38fed540540928f47f
Andrea <andrea.giordano5@studio.unibo.it>
2020-09-13 11:33:55 +0200

    handle roomInfo and error

--- src/main/scala/org/riskala/controller/actors/PlayerRoomBehavior.scala
+++ src/main/scala/org/riskala/controller/actors/PlayerRoomBehavior.scala
@@ -3,8 +3,12 @@ package org.riskala.controller.actors
 import akka.actor
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
+import akka.http.scaladsl.model.ws.TextMessage
 import org.riskala.controller.actors.PlayerMessages.{ErrorMessage, GameReferent, LobbyReferent, PlayerMessage, RoomInfoMessage, SocketMessage}
 import org.riskala.model.ModelMessages.RoomMessage
+import org.riskala.utils.Parser
+import org.riskala.view.messages.ToClientMessages.RoomInfo
+import org.riskala.view.messages.ToClientMessages
 
 object PlayerRoomBehavior {
   def apply(username: String, room: ActorRef[RoomMessage], socket: actor.ActorRef): Behavior[PlayerMessage] = {
@@ -21,14 +25,31 @@ object PlayerRoomBehavior {
       playerRoomBehavior(nextUsername,nextRoom,nextSocket)
 
     message match {
-      case SocketMessage(payload) => nextBehavior()
-      case RoomInfoMessage(roomInfo) => nextBehavior()
-      case LobbyReferent(lobby) => nextBehavior()
-      case GameReferent(game) => nextBehavior()
+      case SocketMessage(payload) =>
+        //TODO
+        nextBehavior()
+
+      case RoomInfoMessage(roomInfo) =>
+        context.log.info(s"PlayerActor of $username received RoomInfoMessage")
+        socket ! TextMessage(Parser.wrap("RoomInfo",roomInfo,RoomInfo.RoomInfoCodecJson.Encoder))
+        nextBehavior()
+
+      case LobbyReferent(lobby) =>
+        //TODO
+        nextBehavior()
+      case GameReferent(game) =>
+        //TODO
+        nextBehavior()
       //case ReadyMessage() => nextBehavior()
       //case LeaveMessage() => nextBehavior()
       //case LogoutMessage() => nextBehavior()
-      case ErrorMessage(error) => nextBehavior()
+      case errorMessage: PlayerMessages.ErrorMessage =>
+        context.log.info(s"PlayerActor of $username received ErrorMessage")
+        val clientError = ToClientMessages.ErrorMessage(errorMessage.error)
+        socket ! TextMessage(Parser.wrap("ErrorMessage",
+          clientError,
+          ToClientMessages.ErrorMessage.ErrorCodecJson.Encoder))
+        nextBehavior()
     }
   }
 }
