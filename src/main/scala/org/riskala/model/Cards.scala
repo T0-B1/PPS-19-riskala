a9f81817b203cbafb4afdea8e25da0ad748eea26
alessia <alessia.cerami@studio.unibo.it>
2020-09-15 15:04:27 +0200

    added generation card

--- src/main/scala/org/riskala/model/Cards.scala
+++ src/main/scala/org/riskala/model/Cards.scala
@@ -12,33 +12,39 @@ object Cards {
   sealed trait Card { val bonus: Int }
 
   /**
-   * Artillery card data type
-   * @param bonus: the bonus associated to this card
+   * Infantry card data type
    * */
-  final case class Artillery(bonus:Int) extends Card
-  object Artillery{
-    implicit def ArtilleryCodecJson =
-      casecodec1(Artillery.apply,Artillery.unapply)("bonus")
+  final case class Infantry() extends Card { val bonus:Int = 3 }
+  object Infantry{
+    implicit def InfantryCodecJson =
+      casecodec1(Infantry.apply,Infantry.unapply)("bonus")
   }
 
   /**
    * Cavalry card data type
-   * @param bonus: the bonus associated to this card
    * */
-  final case class Cavalry(bonus:Int) extends Card
+  final case class Cavalry() extends Card { val bonus:Int = 5 }
   object Cavalry{
     implicit def CavalryCodecJson =
       casecodec1(Cavalry.apply,Cavalry.unapply)("bonus")
   }
 
   /**
-   * Infantry card data type
-   * @param bonus: the bonus associated to this card
+   * Artillery card data type
    * */
-  final case class Infantry(bonus:Int) extends Card
-  object Infantry{
-    implicit def InfantryCodecJson =
-      casecodec1(Infantry.apply,Infantry.unapply)("bonus")
+  final case class Artillery() extends Card { val bonus:Int = 7 }
+  object Artillery{
+    implicit def ArtilleryCodecJson =
+      casecodec1(Artillery.apply,Artillery.unapply)("bonus")
+  }
+
+  def generateCard(): Card = {
+    val rng = scala.util.Random
+    rng.nextInt(3) match {
+      case 0 => Infantry()
+      case 1 => Cavalry()
+      case 2 => Artillery()
+    }
   }
 }
 
