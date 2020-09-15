260b135133a28a984de362cf378a1a2ce8e6c9f5
Andrea <andrea.giordano5@studio.unibo.it>
2020-09-15 17:55:03 +0200

    refactor objectives

--- src/main/scala/org/riskala/model/Objectives.scala
+++ src/main/scala/org/riskala/model/Objectives.scala
@@ -4,26 +4,19 @@ import org.riskala.model.State.State
 
 object Objectives {
 
-  sealed trait Objective {
-    val info: String
-    val states: Seq[State]
-  }
+  final case class Objective(states: Seq[State], info: String)
 
-  final case class WholeMap(override val states: Seq[State]) extends Objective {
-    override val info: String = "Conquer all the states"
-  }
-
-  final case class TwoRegions(override val states: Seq[State],
-                              private val region1Name: String,
-                              private val region2Name: String) extends Objective {
-    override val info: String = s"Conquer the states of region $region1Name and $region2Name"
-  }
-
-  def generateObjective(map: Map): Objective = {
-    val rng = scala.util.Random
-    rng.nextInt(2) match {
-      case 0 => WholeMap(map.states)
-      case 1 => WholeMap(map.states) //TODO replace with TwoRegions
+  def generateRandomObjective(map: Map, numberOfPlayer: Int): Objective = {
+    numberOfPlayer match {
+      case _ if numberOfPlayer < 4 => Objective(map.states,"Conquer all states")
+      case _ =>
+        /*val rng = scala.util.Random
+        rng.nextInt(2) match {
+          case 0 => Objective(map.states)
+          case 1 => Objective(map.states)
+        }*/
+        Objective(map.states,"Conquer all states")//TODO replace with some regions
     }
   }
+  
 }
