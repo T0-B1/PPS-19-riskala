82a1b6896fb45c9375dc9223026f6c2b5eb28dd2
alessia <alessia.cerami@studio.unibo.it>
2020-09-15 23:41:08 +0200

    client game facade first impl

---
+++ src/main/scala/org/riskala/view/game/GameFacade.scala
@@ -0,0 +1,12 @@
+package org.riskala.view.game
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+
+@js.native
+@JSGlobal
+class GameFacade {
+  def cleanGame(): Unit = js.native
+  def getStateInfo(name: String): Unit = js.native
+  def addPlayers(player:String): Unit = js.native
+}
