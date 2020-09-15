6b74c9622a4d793bcffd8f5d4cbd59fd8eb09f94
alessia <alessia.cerami@studio.unibo.it>
2020-09-15 14:44:06 +0200

    added json codec

--- src/main/scala/org/riskala/model/Cards.scala
+++ src/main/scala/org/riskala/model/Cards.scala
@@ -1,5 +1,9 @@
 package org.riskala.model
 
+import argonaut.Argonaut.casecodec1
+
+import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
+
 /**
  * Structure of Cards
  * */
@@ -12,18 +16,29 @@ object Cards {
    * @param bonus: the bonus associated to this card
    * */
   final case class Artillery(bonus:Int) extends Card
+  object Artillery{
+    implicit def ArtilleryCodecJson =
+      casecodec1(Artillery.apply,Artillery.unapply)("bonus")
+  }
 
   /**
    * Cavalry card data type
    * @param bonus: the bonus associated to this card
    * */
   final case class Cavalry(bonus:Int) extends Card
+  object Cavalry{
+    implicit def CavalryCodecJson =
+      casecodec1(Cavalry.apply,Cavalry.unapply)("bonus")
+  }
 
   /**
    * Infantry card data type
    * @param bonus: the bonus associated to this card
    * */
   final case class Infantry(bonus:Int) extends Card
-
+  object Infantry{
+    implicit def InfantryCodecJson =
+      casecodec1(Infantry.apply,Infantry.unapply)("bonus")
+  }
 }
 
