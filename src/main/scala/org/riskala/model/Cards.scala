d83a51a82e987ed8bbaefb0fba7b83ce30046f05
alessia <alessia.cerami@studio.unibo.it>
2020-09-15 12:38:35 +0200

    Added javadoc

--- src/main/scala/org/riskala/model/Cards.scala
+++ src/main/scala/org/riskala/model/Cards.scala
@@ -1,13 +1,28 @@
 package org.riskala.model
 
+/**
+ * Structure of Cards
+ * */
 object Cards {
 
   sealed trait Card { val bonus: Int }
 
+  /**
+   * Artillery card data type
+   * @param bonus: the bonus associated to this card
+   * */
   final case class Artillery(bonus:Int) extends Card
 
+  /**
+   * Cavalry card data type
+   * @param bonus: the bonus associated to this card
+   * */
   final case class Cavalry(bonus:Int) extends Card
 
+  /**
+   * Infantry card data type
+   * @param bonus: the bonus associated to this card
+   * */
   final case class Infantry(bonus:Int) extends Card
 
 }
