75f82ee8274660b16cef1cb283ea86e3337e4ae4
Andrea <andrea.giordano5@studio.unibo.it>
2020-09-15 12:45:43 +0200

    WholeMap objective

--- src/main/scala/org/riskala/model/Objectives.scala
+++ src/main/scala/org/riskala/model/Objectives.scala
@@ -9,4 +9,8 @@ object Objectives {
     val states: Seq[State]
   }
 
+  final case class WholeMap(override val states: Seq[State]) extends Objective {
+    override val info: String = "Conquer all the states"
+  }
+
 }
