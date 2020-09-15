03e5f7a7f94d6dbad674a0a795e83dfd1ad0d2ec
alessia <alessia.cerami@studio.unibo.it>
2020-09-15 12:27:38 +0200

    cards implementation

--- src/main/scala/org/riskala/model/Cards.scala
+++ src/main/scala/org/riskala/model/Cards.scala
@@ -3,6 +3,12 @@ package org.riskala.model
 object Cards {
 
   sealed trait Card { val bonus: Int }
-  
+
+  final case class Artillery(bonus:Int) extends Card
+
+  final case class Cavalry(bonus:Int) extends Card
+
+  final case class Infantry(bonus:Int) extends Card
+
 }
 
