65cbc8ff1ab481b27c8d5e51c468d62b37deaee9
alessia <alessia.cerami@studio.unibo.it>
2020-09-17 18:45:50 +0200

    added methods called on js

--- src/main/scala/org/riskala/view/game/GameFacade.scala
+++ src/main/scala/org/riskala/view/game/GameFacade.scala
@@ -1,12 +1,20 @@
 package org.riskala.view.game
 
+import org.riskala.model.{MapGeography, PlayerState}
+
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSGlobal
 
 @js.native
 @JSGlobal
 class GameFacade extends js.Object {
-  def cleanGame(): Unit = js.native
-  def getStateInfo(name: String): Unit = js.native
-  def addPlayers(player:String): Unit = js.native
+  def addPlayer(player:String, myTurn: Boolean): Unit = js.native
+  def setMap(map: MapGeography): Unit = js.native //Così chiamo getNeighbors
+  def cleanPlayerState(): Unit = js.native
+  def addPlayerState(playerState: PlayerState): Unit = js.native
+  def loadObjectives(objective: String): Unit = js.native
+  def loadCard(infantry: Int, cavalry: Int, artillery: Int): Unit = js.native
+
+
+  def updateInfo(playerTurn: String): Unit = js.native //Cosa devo prendere?
 }
