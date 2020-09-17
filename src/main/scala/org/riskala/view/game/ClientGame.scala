05ec6dca41a89259c502c69b2e3280a43fa3d3e8
alessia <alessia.cerami@studio.unibo.it>
2020-09-17 18:46:40 +0200

    Game client wrapped msg

--- src/main/scala/org/riskala/view/game/ClientGame.scala
+++ src/main/scala/org/riskala/view/game/ClientGame.scala
@@ -1,8 +1,17 @@
 package org.riskala.view.game
 
+import argonaut.Argonaut.{ToJsonIdentity, nospace}
+import org.riskala.utils.Parser
+import org.riskala.view.messages.FromClientMessages.ActionMessage
+import org.riskala.view.messages.WrappedMessage
+
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("ClientGame")
 object ClientGame {
 
+  @JSExport
+  def getEmptyMsgWrapped(typeMessage: String): String =  { //per leaveMsg, logoutMsg e endTurnMsg
+    WrappedMessage("ReadyMessage", "").asJson.pretty(nospace)
+  }
 }
