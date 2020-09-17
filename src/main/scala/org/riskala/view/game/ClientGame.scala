fec3c76eba8f9826b9b337855331ec422e48d4bd
alessia <alessia.cerami@studio.unibo.it>
2020-09-17 18:54:31 +0200

    added action wrapped message

--- src/main/scala/org/riskala/view/game/ClientGame.scala
+++ src/main/scala/org/riskala/view/game/ClientGame.scala
@@ -1,7 +1,6 @@
 package org.riskala.view.game
 
 import argonaut.Argonaut.{ToJsonIdentity, nospace}
-import org.riskala.utils.Parser
 import org.riskala.view.messages.FromClientMessages.ActionMessage
 import org.riskala.view.messages.WrappedMessage
 
@@ -12,6 +11,13 @@ object ClientGame {
 
   @JSExport
   def getEmptyMsgWrapped(typeMessage: String): String =  { //per leaveMsg, logoutMsg e endTurnMsg
-    WrappedMessage("ReadyMessage", "").asJson.pretty(nospace)
+    WrappedMessage(typeMessage, "").asJson.pretty(nospace)
   }
+
+  @JSExport
+  def getActionMsgWrapped(from: String, to: String, troops: Int): String =  {
+    WrappedMessage("ActionMessage",
+      ActionMessage(from, to, troops).asJson.pretty(nospace)).asJson.pretty(nospace)
+  }
+
 }
