1677bf8a03592d18490740974e86dac31460c1a1
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-17 03:15:02 +0200

    Refactoring events

--- src/main/scala/org/riskala/Model/events/Events.scala
+++ src/main/scala/org/riskala/Model/events/Events.scala
@@ -1,31 +1,34 @@
 package org.riskala.model.events
 
-import org.riskala.model.{Player, State}
+import org.riskala.model.Player
 import org.riskala.model.State.State
 
-object Events {
+sealed trait Event
 
-  sealed trait Event
+final case class Attack(from: State,
+                       to: State,
+                       attacking: Int,
+                       attackingPassed: Int,
+                       defendingCasualties: Int)
+                  extends Event
 
-  final case class Attack(from: State,
-                         to: State,
-                         attacking: Int,
-                         attackingPassed: Int,
-                         defendingCasualties: Int)
+final case class Move(from: State,
+                      to: State,
+                      troops: Int)
+                  extends Event
 
-  final case class Move(from: State,
-                        to: State,
+final case class Deploy(to: State,
                         troops: Int)
+                  extends Event
 
-  final case class Deploy(to: State,
-                          troops: Int)
+// TODO use card
+final case class Draw(player: Player,
+                      card: Unit)
+                  extends Event
 
-  // TODO use card
-  final case class Draw(player: Player,
-                        card: Unit)
+// TODO use card
+final case class RedeemBonus(player: Player,
+                        cardBonus: Unit)
+                  extends Event
 
-  // TODO use card
-  final case class RedeemBonus(player: Player,
-                          cardBonus: Unit)
 
-}
