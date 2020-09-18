aa8d90019336e20b6143b24c9cf6e2e493a4e5ed
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-18 05:51:38 +0200

    Implemented snapshot of a new game

--- src/main/scala/org/riskala/model/game/GameSnapshot.scala
+++ src/main/scala/org/riskala/model/game/GameSnapshot.scala
@@ -4,6 +4,7 @@ import org.riskala.model.Cards.Cards
 import org.riskala.model.Objectives.Objective
 import org.riskala.model.{Player, PlayerState}
 import org.riskala.utils.Utils
+import org.riskala.model.Objectives
 
 case class GameSnapshot(players: Seq[Player],
                         scenario: org.riskala.model.Map,
@@ -16,6 +17,9 @@ object GameSnapshot {
   def newGame(players: Seq[Player],
               scenario: org.riskala.model.Map) : GameSnapshot = {
     val geopolitics: Set[PlayerState] = scenario.states.map(s=> PlayerState(s, Utils.randomSetElement[Player](players.toSet), 1))
-
+    val nowPlaying: Player = Utils.randomSetElement(players.toSet)
+    val cards: Map[Player, Set[Cards]] = players.map(p => (p, Set.empty[Cards])).toMap
+    val objectives: Map[Player, Objective] = players.map(p => (p, Objectives.generateRandomObjective(scenario, players.size))).toMap
+    GameSnapshot(players, scenario, geopolitics, nowPlaying, cards, objectives)
   }
 }
