097776e12bf9103ecb6754656cbe5655b0065b04
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-19 00:56:27 +0200

    Moved cardsProjection

---
+++ src/main/scala/org/riskala/model/eventSourcing/projections/CardsProjection.scala
@@ -0,0 +1,23 @@
+package org.riskala.model.eventSourcing.projections
+
+import org.riskala.model.Cards.Cards
+import org.riskala.model.Player
+import org.riskala.model.eventSourcing.{BonusRedeemed, CardDrawn, Event}
+
+class CardsProjection extends Projection[Map[Player, Seq[Cards]], Event] {
+
+  override def Init: Map[Player, Seq[Cards]] = Map.empty
+
+  override def Update: (Map[Player, Seq[Cards]], Event) => Map[Player, Seq[Cards]] = (cards, ev) => ev match {
+    case drawn: CardDrawn => {
+      val playerCards = cards.getOrElse(drawn.player, Seq.empty[Cards]) :+ drawn.card
+      cards + (drawn.player -> playerCards)
+    }
+    case redeem: BonusRedeemed => {
+      val toRemove = Seq.fill(3)(redeem.cardBonus)
+      val playerCards = cards.getOrElse(redeem.player, Seq.empty[Cards]) diff toRemove
+      cards + (redeem.player -> playerCards)
+    }
+    case _ => cards
+  }
+}
