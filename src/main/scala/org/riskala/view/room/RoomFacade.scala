3ae560a1290d8f11a7aa88552b0740cbf817cf5a
Andrea <andrea.giordano5@studio.unibo.it>
2020-09-18 23:34:52 +0200

    goToGame

--- src/main/scala/org/riskala/view/room/RoomFacade.scala
+++ src/main/scala/org/riskala/view/room/RoomFacade.scala
@@ -8,4 +8,5 @@ class RoomFacade extends js.Object{
   def addPlayers(name:String): Unit = js.native
   def clearPlayer(): Unit = js.native
   def setName(roomName: String): Unit = js.native
+  def goToGame(gameInfo: String): Unit = js.native
 }
