e13ca42fa2a5f7b3e399da09f01609b08175e1b8
alessia <alessia.cerami@studio.unibo.it>
2020-09-18 23:16:15 +0200

    client Game

--- src/main/scala/org/riskala/view/game/ClientGame.scala
+++ src/main/scala/org/riskala/view/game/ClientGame.scala
@@ -2,6 +2,7 @@ package org.riskala.view.game
 
 import argonaut.Argonaut.{ToJsonIdentity, nospace}
 import org.riskala.model.Cards._
+import org.riskala.model.{MapGeography, PlayerState}
 import org.riskala.utils.Parser
 import org.riskala.view.messages.FromClientMessages.{ActionMessage, RedeemBonusMessage}
 import org.riskala.view.messages.ToClientMessages.{ErrorMessage, GameFullInfo, LobbyInfo}
@@ -11,6 +12,9 @@ import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("ClientGame")
 object ClientGame {
+
+  private var map: MapGeography = _
+  private var playerStates: Set[PlayerState] = _
 
   @JSExport
   def getEmptyMsgWrapped(typeMessage: String): String =  { //per leaveMsg, logoutMsg e endTurnMsg
@@ -26,6 +30,51 @@ object ClientGame {
   @JSExport
   def getRedeemBonusMsgWrapped(cardType: Cards): String =  {
     WrappedMessage("RedeemBonusMessage", RedeemBonusMessage(cardType).asJson.pretty(nospace)).asJson.pretty(nospace)
+  }
+
+  @JSExport
+  def neighborClick(clickedState: String, namePlayer: String, mapSelectedState: String, gameFacade: GameFacade): Unit = {
+    if(clickedState equals mapSelectedState) {
+      gameFacade.nameBtn = "Deploy"
+    } else{
+      playerStates.find(_.state == clickedState)
+        .foreach(ps =>
+          if(ps.owner.nickname == namePlayer) gameFacade.nameBtn = "Move" else gameFacade.nameBtn = "Attack")
+    }
+  }
+
+  private def setStateInfo(playerState:PlayerState, gameFacade: GameFacade, myState: Boolean): Unit = {
+    println("setStateInfo")
+    gameFacade.state = playerState.state
+    println("gameFacade.state: " + gameFacade.state)
+    gameFacade.owner = playerState.owner.nickname
+    println("gameFacade.owner: " + gameFacade.owner)
+    gameFacade.troops = playerState.troops
+    println("gameFacade.troops: " + gameFacade.troops)
+    gameFacade.region = map.regions.find(_.states.contains(playerState.state)).map(_.name).getOrElse("")
+    println("gameFacade.region " + gameFacade.region)
+
+    if(myState){
+      gameFacade.visible = true
+      if(gameFacade.troopsToDeploy > 0){
+        gameFacade.addNeighbor(playerState.state, true)
+        map.getNeighbors(playerState.state).foreach(gameFacade.addNeighbor(_, false))
+        gameFacade.maxAvailableTroops = gameFacade.troopsToDeploy
+      } else {
+        map.getNeighbors(playerState.state).foreach(gameFacade.addNeighbor(_, true))
+        gameFacade.maxAvailableTroops = playerState.troops - 1
+      }
+    }
+    gameFacade.visible = false
+  }
+
+  @JSExport
+  def clickedState(nameState: String, namePlayer: String, gameFacade: GameFacade): Unit = {
+    println("nameState: "+ nameState)
+    println("namePlayer: "+ namePlayer)
+    println("playerStates " + playerStates)
+    playerStates.find(_.state == nameState)
+      .foreach(ps => setStateInfo(ps, gameFacade, ps.owner.nickname == namePlayer))
   }
 
   @JSExport
@@ -45,11 +94,15 @@ object ClientGame {
         val gameFullInfo =
           Parser.retrieveMessage(wrappedMsg.payload, GameFullInfo.GameFullInfoCodecJson.Decoder).get
         println("Ended parser retrieve message")
+        map = gameFullInfo.map
+        playerStates = gameFullInfo.playerStates
+        
         gameFullInfo.players.foreach(pl => gameFacade.addPlayer(pl, gameFullInfo.actualPlayer == pl))
-        gameFacade.setMap(gameFullInfo.map)
+        gameFacade.setMap(map)
         gameFacade.cleanPlayerState()
-        gameFullInfo.playerStates.foreach(ps => gameFacade.addPlayerState(ps))
+        playerStates.foreach(ps => gameFacade.addPlayerState(ps))
         gameFacade.loadObjectives(gameFullInfo.personalInfo.objective.info)
+
         val cardOccurrence = gameFullInfo.personalInfo.cards.groupBy(identity).mapValues(_.size)
         gameFacade.setCardInfo(cardOccurrence.getOrElse(Infantry, 0),
           cardOccurrence.getOrElse(Cavalry, 0),
