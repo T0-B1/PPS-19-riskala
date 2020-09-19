f458cfeb6cffdbaacf3e273322c8841309601d5e
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-19 04:25:25 +0200

    Generic projection on the whole game

--- src/main/scala/org/riskala/model/eventSourcing/projections/Projection.scala
+++ src/main/scala/org/riskala/model/eventSourcing/projections/Projection.scala
@@ -1,6 +1,12 @@
 package org.riskala.model.eventSourcing.projections
 
+import org.riskala.model.eventSourcing.{Event, GameSnapshot}
+
 trait Projection[State, Event] {
   def Update: (State, Event) => State
   def Project: (State, Seq[Event]) => State = (state, events) => events.foldLeft(state)(Update)
+}
+
+case class SnapshotGenerator() extends Projection[GameSnapshot, Event]{
+  override def Update: (GameSnapshot, Event) => GameSnapshot = (game, event) => event.happen(game)
 }
