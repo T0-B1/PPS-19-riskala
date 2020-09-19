661fb10daa525c3078cbf45fe983dbcb70006d53
alessia <alessia.cerami@studio.unibo.it>
2020-09-19 18:26:43 +0200

    lobbyInfo

--- src/vuejs/src/components/Lobby/Lobby.vue
+++ src/vuejs/src/components/Lobby/Lobby.vue
@@ -43,6 +43,10 @@
         ClientLobby.handleLobbyMessage(evt.data, vue)
       }
       this.$store.commit('changeHandler', newHandler)
+
+      if(this.$store.state.lobbyInfo !== ''){
+        ClientLobby.setupLobby(this.$store.state.lobbyInfo, this)
+      }
     },
     methods: {
       readSocketMessage() {
@@ -78,10 +82,12 @@
         }
       },
       goToRoom(newRoom){
+        this.$store.commit('changeLobbyInfo', '')
         this.$store.commit('changeRoomInfo', newRoom)
         this.$router.push('/room')
       },
       goToGame(newGame){
+        this.$store.commit('changeLobbyInfo', '')
         this.$store.commit('changeGameInfo', newGame)
         this.$router.push('/game')
       },
