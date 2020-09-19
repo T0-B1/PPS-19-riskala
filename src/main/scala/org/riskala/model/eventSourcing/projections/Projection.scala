2aa9741049843231aec75b846301f970a8269307
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-19 04:19:16 +0200

    Minor

--- src/main/scala/org/riskala/model/eventSourcing/projections/Projection.scala
+++ src/main/scala/org/riskala/model/eventSourcing/projections/Projection.scala
@@ -2,5 +2,5 @@ package org.riskala.model.eventSourcing.projections
 
 trait Projection[State, Event] {
   def Update: (State, Event) => State
-  def Project: (State, Seq[Event]) => State = (initState, events) => events.foldLeft(initState)(Update)
+  def Project: (State, Seq[Event]) => State = (state, events) => events.foldLeft(state)(Update)
 }
