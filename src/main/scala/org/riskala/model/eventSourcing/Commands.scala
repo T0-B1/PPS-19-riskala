df3854099310da64d0abee96e62315c4c906d67c
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-19 16:47:54 +0200

    Removed old code

--- src/main/scala/org/riskala/model/eventSourcing/Commands.scala
+++ src/main/scala/org/riskala/model/eventSourcing/Commands.scala
@@ -118,26 +118,6 @@ final case class EndTurn(player: Player)
 
 object Command {
 
-  // TODO decompose and move single behavior into relative commands
-  // TODO provide an overload of behavior which takes a snapshot instead of a Seq[Events]
-
-  def behaviorOf(command: Command) : Seq[Event] => Seq[Event] = {
-    command match {
-      // TODO use projection and/or overload above to verify state before performing behavior
-      // TODO maybe introduce scalaz validation
-      case Attack(from, to, troops) =>
-        _ => Seq(Battle(null,null,0,0,0))
-      case MoveTroops(from, to, troops) =>
-        _ => Seq(TroopsMoved(null,null,0))
-      case Deploy(to, troops) =>
-        _ => Seq(TroopsDeployed(null,0))
-      case RedeemBonus(player, cardBonus) =>
-        _ => Seq(BonusRedeemed(player, cardBonus))
-      case EndTurn(player) =>
-        _ => Seq(TurnEnded(player))
-    }
-  }
-
   def checkTurn(player: Player, game: GameSnapshot): FeasibilityReport = {
     game.nowPlaying match {
       case p if p.equals(player) => FeasibilityReport(true, None)
