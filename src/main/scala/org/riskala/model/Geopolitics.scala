155c451bb3986b052c8695f4c144b8e8cfb22d2e
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-19 13:18:38 +0200

    Removed old geopolitics object methods

--- src/main/scala/org/riskala/model/Geopolitics.scala
+++ src/main/scala/org/riskala/model/Geopolitics.scala
@@ -50,41 +50,5 @@ object Geopolitics {
   implicit def states2geopolitics(states: Set[PlayerState]): Geopolitics = Geopolitics(states)
 
   implicit def geopolitics2states(geopolitics: Geopolitics): Set[PlayerState] = geopolitics.states
-
-  def updateGeopolitics(state: PlayerState, geopolitics: Set[PlayerState]): Set[PlayerState] = {
-    geopolitics.filterNot(p => p.state.equals(state.state)) + state
-  }
-
-  def getPlayerState(state: State, geopolitics: Set[PlayerState]): Option[PlayerState] = {
-    geopolitics.collectFirst({ case p if p.state.equals(state) => p })
-  }
-
-  def updateStateOwner(state: State, newOwner: Player, geopolitics: Set[PlayerState]) = {
-    val ps = getPlayerState(state, geopolitics)
-    if(ps.isEmpty)
-      geopolitics
-    else
-      updateGeopolitics(ps.get.copy(owner = newOwner), geopolitics)
-  }
-
-  def modifyStateTroops(state: State, troopsDelta: Int, geopolitics: Set[PlayerState]) = {
-    alterStateTroops(state, troopsDelta, geopolitics, true)
-  }
-
-  def setStateTroops(state: State, troops: Int, geopolitics: Set[PlayerState]) = {
-    alterStateTroops(state, troops, geopolitics, false)
-  }
-
-  private def alterStateTroops(state: State, troops: Int, geopolitics: Set[PlayerState], additive: Boolean) = {
-    val ps = getPlayerState(state, geopolitics)
-    if(ps.isEmpty)
-      geopolitics
-    else {
-      var t = troops
-      if(additive)
-        t = ps.get.troops + troops
-      updateGeopolitics(ps.get.copy(troops = t), geopolitics)
-    }
-  }
-
+  
 }
