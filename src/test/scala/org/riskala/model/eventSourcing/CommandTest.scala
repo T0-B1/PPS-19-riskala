17826edf80e1121fb002b7b19d9985046db22383
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-19 11:54:38 +0200

    Command unfeasible when not your turn test

--- src/test/scala/org/riskala/model/eventSourcing/CommandTest.scala
+++ src/test/scala/org/riskala/model/eventSourcing/CommandTest.scala
@@ -2,16 +2,32 @@ package org.riskala.model.eventSourcing
 
 
 import org.junit.runner.RunWith
+import org.riskala.model
+import org.riskala.model.{Cards, Player}
+import org.riskala.utils.MapLoader
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class CommandTest extends AnyWordSpec {
 
+  val p1: Player = Player("p1", "green")
+  val p2: Player = Player("p2", "blue")
+  val p3: Player = Player("p3", "red")
+  val players = Seq(p1, p2, p3)
+  val scenario: model.Map = MapLoader.loadMap("italy").get
+  val initialSnapshot: GameSnapshot = GameSnapshot.newGame(players, scenario)
+  val attackingState = "Emilia-Romagna"
+  val defendingState = "Toscana"
+
   "No command except EndTurn" should {
     "be feasible" when {
       "is not your turn" in {
-
+        val game = initialSnapshot.copy(nowPlaying = Player("foo", ""))
+        assert(!Attack(attackingState, defendingState, 0).feasibility(game).feasible)
+        assert(!MoveTroops(attackingState, defendingState, 0).feasibility(game).feasible)
+        assert(!Deploy(attackingState,0).feasibility(game).feasible)
+        assert(!RedeemBonus(p1, Cards.Artillery).feasibility(game).feasible)
       }
     }
   }
