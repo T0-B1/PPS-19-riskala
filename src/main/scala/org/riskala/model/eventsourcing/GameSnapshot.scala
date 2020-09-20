977bb79677a1ff8fe36e310059720548975835fe
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-20 18:09:27 +0200

    Added turn number in GameSnapshot

--- src/main/scala/org/riskala/model/eventsourcing/GameSnapshot.scala
+++ src/main/scala/org/riskala/model/eventsourcing/GameSnapshot.scala
@@ -9,6 +9,7 @@ case class GameSnapshot(players: Seq[Player],
                         scenario: MapGeography,
                         geopolitics: Geopolitics,
                         nowPlaying: Player,
+                        turn: Int,
                         deployableTroops: Int,
                         cards: Map[Player, Seq[Cards]],
                         objectives: Map[Player, Objective],
@@ -22,7 +23,7 @@ object GameSnapshot {
     val cards: Map[Player, Seq[Cards]] = players.map(p => (p, Seq.empty[Cards])).toMap
     val objectives: Map[Player, Objective] = players.map(p => (p, Objectives.generateRandomObjective(scenario, players.size))).toMap
     val deployable = statesOwned(nowPlaying, geopolitics)
-    GameSnapshot(players, scenario, geopolitics, nowPlaying, deployable, cards, objectives)
+    GameSnapshot(players, scenario, geopolitics, nowPlaying, 0, deployable, cards, objectives)
   }
 
   def newGame(players: Seq[Player],
