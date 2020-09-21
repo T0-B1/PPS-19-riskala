8d3be5796f3f07fba9517c730281e4911c03d9b7
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-21 04:33:40 +0200

    Moved utils

--- src/main/scala/org/riskala/utils/Utils.scala
+++ src/main/scala/org/riskala/utils/Utils.scala
@@ -1,5 +1,8 @@
 package org.riskala.utils
 
+import java.io.FileNotFoundException
+import java.util.Properties
+
 import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
 import akka.actor.typed.scaladsl.AskPattern._
@@ -8,8 +11,21 @@ import akka.util.Timeout
 import scala.concurrent.{Await, ExecutionContextExecutor, Future}
 import scala.reflect.ClassTag
 import scala.concurrent.duration._
+import scala.io.Source
 
 object Utils{
+
+  val PROPS_PATH = "/test.properties"
+
+  def loadPropertiesFromResources(): Properties = {
+    val properties: Properties = new Properties()
+    val url = getClass.getResource(PROPS_PATH)
+    if (url != null)
+      properties.load(Source.fromURL(url).bufferedReader())
+    else
+      throw new FileNotFoundException(s"Properties file at path $PROPS_PATH cannot be loaded");
+    properties
+  }
 
   def askReceptionistToFind[T:ClassTag](key: String)(implicit system: ActorSystem[Nothing], timeout: Timeout = 3.seconds) : Set[ActorRef[T]] = {
     implicit val executionContext: ExecutionContextExecutor = system.executionContext
