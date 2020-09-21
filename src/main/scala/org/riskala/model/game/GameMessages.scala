9b47e58b403015e500e2c09045b687d1a7d16a76
alessia <alessia.cerami@studio.unibo.it>
2020-09-21 18:15:07 +0200

    GameMessages javadoc

--- src/main/scala/org/riskala/model/game/GameMessages.scala
+++ src/main/scala/org/riskala/model/game/GameMessages.scala
@@ -13,43 +13,62 @@ import scala.collection.immutable.Queue
 object GameMessages {
 
   /**
-   * Message sent when an actor wants to join this game
+   * Message sent when a player wants to join this game
+   * @param actor The player that wants to join the game
    * */
   case class JoinGame(actor: ActorRef[PlayerMessage]) extends GameMessage
 
   /**
    * Message sent when an actor wants to leave this game
+   * @param actor The actor that wants to leave the game
    * */
   case class Leave(actor: ActorRef[PlayerMessage]) extends GameMessage
 
   /**
    * Message sent when an actor wants to do an attack.
+   * @param playerName The actual player
+   * @param from The state of departure
+   * @param to The arrival state
+   * @param troops The troops that the player decided to use for the action
    * */
   case class ActionAttack(playerName: String, from: String, to: String, troops: Int) extends GameMessage
 
   /**
    * Message sent when an actor wants to do a move.
+   * @param playerName The actual player
+   * @param from The state of departure
+   * @param to The arrival state
+   * @param troops The troops that the player decided to use for the action
    * */
   case class ActionMove(playerName: String, from: String, to: String, troops: Int) extends GameMessage
 
   /**
    * Message sent when an actor wants to do a deploy.
    * Action type: Move, Deploy or Attack
+   * @param playerName The actual player
+   * @param from The state of departure
+   * @param to The arrival state
+   * @param troops The troops that the player decided to use for the action
    * */
   case class ActionDeploy(playerName: String, from: String, to: String, troops: Int) extends GameMessage
 
   /**
    * Message sent when an actor wants to redeem bonus
+   * @param playerName The actual player
+   * @param card The random card redeemed
    * */
   case class RedeemBonus(playerName: String, card: Cards) extends GameMessage
 
   /**
    * Message sent when an actor ends his turn
+   * @param playerName The player who ended his turn
    * */
   case class EndTurn(playerName: String) extends GameMessage
 
   /**
    * Message sent when an actor need all the game info
+   * @param playerName The name of the possible asking actor
+   * @param actor The actor who receives the game information
    * */
-  case class GetFullInfo(playerName: String, replyTo: ActorRef[PlayerMessage]) extends GameMessage
+  case class GetFullInfo(playerName: String, actor: ActorRef[PlayerMessage]) extends GameMessage
 }
