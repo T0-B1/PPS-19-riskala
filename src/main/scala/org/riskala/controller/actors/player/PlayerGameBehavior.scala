97b60d73ab66a64b8c30d99012b689328d8f55c8
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2020-09-22 20:17:01 +0200

    Fixed imports

--- src/main/scala/org/riskala/controller/actors/player/PlayerGameBehavior.scala
+++ src/main/scala/org/riskala/controller/actors/player/PlayerGameBehavior.scala
@@ -4,7 +4,7 @@ import akka.actor
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.http.scaladsl.model.ws.TextMessage
-import org.riskala.controller.actors.player.PlayerMessages.PlayerMessage
+import org.riskala.controller.actors.player.PlayerMessages._
 import org.riskala.model.ModelMessages.{GameMessage, Logout}
 import org.riskala.controller.actors.game.GameMessages._
 import org.riskala.utils.Parser
